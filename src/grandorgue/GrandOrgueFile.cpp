c0e5a762bd9506bf6539e57738d71a55e9f25e41
nappleton <nappleton@69509848-2a73-49d5-a893-c4d8c934025a>
2011-08-11 13:08:00 +0000

    Include BLOCKS_PER_FRAME in the cache hash, as it affectes the saved data structures (MK)
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@198 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GrandOrgueFile.cpp
+++ src/grandorgue/GrandOrgueFile.cpp
@@ -135,6 +135,8 @@ void GrandOrgueFile::GenerateCacheHash(unsigned char hash[20])
 	SHA1_Update(&ctx, &len, sizeof(len));
 	len = sizeof(GOrgueReleaseAlignTable);
 	SHA1_Update(&ctx, &len, sizeof(len));
+	len = BLOCKS_PER_FRAME;
+	SHA1_Update(&ctx, &len, sizeof(len));
 
 	SHA1_Final(hash, &ctx);
 }
