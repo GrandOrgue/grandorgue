01cfa0cbe2400050437bdb603562b124257ec4b1
Nick Appleton <nick@appletonaudio.com>
2011-09-15 09:12:08 +0000

    Allow ODF to restyle Setter buttons (MK)
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@315 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GOGUISetterButton.cpp
+++ src/grandorgue/GOGUISetterButton.cpp
@@ -25,6 +25,7 @@
 #include "GOGUIDisplayMetrics.h"
 #include "GOrgueSetterButton.h"
 #include "MIDIEventDialog.h"
+#include "IniFileConfig.h"
 
 GOGUISetterButton::GOGUISetterButton(GOGUIPanel* panel, GOrgueSetterButton* control):
 	GOGUIControl(panel, control),
@@ -34,41 +35,66 @@ GOGUISetterButton::GOGUISetterButton(GOGUIPanel* panel, GOrgueSetterButton* cont
 	m_DispLabelColour(0,0,0),
 	m_DispDrawstopRow(0),
 	m_DispDrawstopCol(0),
-	m_DispImageNum(0)
+	m_DispImageNum(0),
+	m_Piston(false)
 {
 }
 
-void GOGUISetterButton::Init(unsigned x_pos, unsigned y_pos)
+void GOGUISetterButton::Init(IniFileConfig& cfg, unsigned x_pos, unsigned y_pos, wxString group, bool IsPiston)
 {
-	m_DispKeyLabelOnLeft = true;
-	m_DispLabelColour = wxColour(0x80, 0x00, 0x00);
-	m_DispLabelFontSize = 7;
-	m_DispDrawstopRow = y_pos;
-	m_DispDrawstopCol = x_pos;
-	m_DispImageNum = 1;
+	m_Piston = cfg.ReadBoolean(group, wxT("DisplayAsButton"), false, IsPiston);
+	m_DispKeyLabelOnLeft = cfg.ReadBoolean(group, wxT("DispKeyLabelOnLeft"), false, true);
+	m_DispLabelColour = cfg.ReadColor(group, wxT("DispLabelColour"), false, wxT("Dark Red"));
+	m_DispLabelFontSize = cfg.ReadFontSize(group, wxT("DispLabelFontSize"), false, wxT("normal"));
+	m_DispImageNum = cfg.ReadInteger(group, wxT("DispImageNum"), 1, 2, false, 1);
+	if (!m_Piston)
+	{
+		m_DispDrawstopRow = cfg.ReadInteger(group, wxT("DispDrawstopRow"), 1, 99 + m_metrics->NumberOfExtraDrawstopRowsToDisplay(), false, y_pos);
+		m_DispDrawstopCol = cfg.ReadInteger(group, wxT("DispDrawstopCol"), 1, m_DispDrawstopRow > 99 ? m_metrics->NumberOfExtraDrawstopColsToDisplay() : m_metrics->NumberOfDrawstopColsToDisplay(), false, x_pos);
+	}
+	else
+	{
+		m_DispDrawstopRow = cfg.ReadInteger(group, wxT("DispButtonRow"), 1, 99 + m_metrics->NumberOfExtraButtonRows(), false, y_pos);
+		m_DispDrawstopCol = cfg.ReadInteger(group, wxT("DispButtonCol"), 1, m_metrics->NumberOfButtonCols(), false, x_pos);
+		m_DispImageNum--;
+	}
 
 	int x, y;
-	m_metrics->GetDrawstopBlitPosition(m_DispDrawstopRow, m_DispDrawstopCol, &x, &y);
-	m_BoundingRect = wxRect(x, y, 65, 65);
+	if (m_Piston)
+	{
+		m_metrics->GetPushbuttonBlitPosition(m_DispDrawstopRow, m_DispDrawstopCol, &x, &y);
+		m_BoundingRect = wxRect(x, y, 30, 30);
+	}
+	else
+	{
+		m_metrics->GetDrawstopBlitPosition(m_DispDrawstopRow, m_DispDrawstopCol, &x, &y);
+		m_BoundingRect = wxRect(x, y, 65, 65);
+	}
 }
 
 void GOGUISetterButton::Draw(wxDC* dc)
 {
 	wxRect rect(m_BoundingRect.GetX(), m_BoundingRect.GetY() + 1, m_BoundingRect.GetWidth(), m_BoundingRect.GetHeight() - 1);
-	wxBitmap* bmp = m_panel->GetImage(((m_DispImageNum - 1) << 1) + (m_button->IsEngaged() ? 1 : 0));
+	wxBitmap* bmp;
+	if (m_Piston)
+		bmp = m_panel->GetImage(m_DispImageNum + 4 + (m_button->IsEngaged() ? 2 : 0));
+	else
+		bmp = m_panel->GetImage(((m_DispImageNum - 1) << 1) + (m_button->IsEngaged() ? 1 : 0));
 	dc->DrawBitmap(*bmp, m_BoundingRect.GetX(), m_BoundingRect.GetY(), true);
 	dc->SetTextForeground(m_DispLabelColour);
 	wxFont font = m_metrics->GetControlLabelFont();
 	font.SetPointSize(m_DispLabelFontSize);
 	dc->SetFont(font);
-	dc->DrawLabel(m_panel->WrapText(dc, m_button->GetName(), 51), rect, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
+	dc->DrawLabel(m_panel->WrapText(dc, m_button->GetName(), m_Piston ? 28 : 51), rect, wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
 }
 
 void GOGUISetterButton::HandleMousePress(int x, int y, bool right)
 {
 	if (!m_BoundingRect.Contains(x, y))
 		return;
-	if ((m_BoundingRect.GetX() + 32 - x) * (m_BoundingRect.GetX() + 32 - x) + (m_BoundingRect.GetY() + 32 - y) * (m_BoundingRect.GetY() + 32 - y) > 1024)
+	if (!m_Piston && (m_BoundingRect.GetX() + 32 - x) * (m_BoundingRect.GetX() + 32 - x) + (m_BoundingRect.GetY() + 32 - y) * (m_BoundingRect.GetY() + 32 - y) > 1024)
+		return;
+	if (m_Piston && (m_BoundingRect.GetX() + 15 - x) * (m_BoundingRect.GetX() + 15 - x) + (m_BoundingRect.GetY() + 15 - y) * (m_BoundingRect.GetY() + 15 - y) > 255)
 		return;
 	if (right)
 	{
