86c75e1bc59204c2b5c5ccc725bb01c0a220e691
nappleton <nappleton@69509848-2a73-49d5-a893-c4d8c934025a>
2011-09-17 12:46:06 +0000

    Style changes to variable naming and minor cleanups.
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@331 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GOSoundProvider.cpp
+++ src/grandorgue/GOSoundProvider.cpp
@@ -28,12 +28,12 @@
 
 GOSoundProvider::GOSoundProvider()
 {
-	memset(&m_attack, 0, sizeof(m_attack));
-	memset(&m_loop, 0, sizeof(m_loop));
-	memset(&m_release, 0, sizeof(m_release));
+	memset(&m_Attack, 0, sizeof(m_Attack));
+	memset(&m_Loop, 0, sizeof(m_Loop));
+	memset(&m_Release, 0, sizeof(m_Release));
 	m_Channels = 0;
-	ra_amp = 0;
-	ra_shift = 0;
+	m_ScaleAmp = 0;
+	m_ScaleShift = 0;
 	m_SampleRate = 0;
 }
 
@@ -44,72 +44,102 @@ GOSoundProvider::~GOSoundProvider()
 
 void GOSoundProvider::ClearData()
 {
-	FREE_AND_NULL(m_attack.data);
-	FREE_AND_NULL(m_loop.data);
-	FREE_AND_NULL(m_release.data);
-	DELETE_AND_NULL(m_release.release_aligner);
+	FREE_AND_NULL(m_Attack.data);
+	FREE_AND_NULL(m_Loop.data);
+	FREE_AND_NULL(m_Release.data);
+	DELETE_AND_NULL(m_Release.release_aligner);
+	memset(&m_Attack, 0, sizeof(m_Attack));
+	memset(&m_Loop, 0, sizeof(m_Loop));
+	memset(&m_Release, 0, sizeof(m_Release));
+}
+
+bool GOSoundProvider::LoadCacheAudioSection
+	(wxInputStream* cache, AUDIO_SECTION* section, bool load_align_tracker)
+{
+
+	cache->Read(section, sizeof(AUDIO_SECTION));
+	section->release_aligner = NULL;
+	section->data = NULL;
+	if (cache->LastRead() != sizeof(AUDIO_SECTION))
+		return false;
+
+	section->data = (unsigned char*)malloc(section->alloc_size);
+	if (section->data == NULL)
+		throw (wxString)_("< out of memory allocating samples");
+
+	cache->Read(section->data, section->alloc_size);
+	if (cache->LastRead() != section->alloc_size)
+		return false;
+
+	if (load_align_tracker)
+	{
+
+		bool has_align_tracker;
+		cache->Read(&has_align_tracker, sizeof(has_align_tracker));
+		if (cache->LastRead() != sizeof(has_align_tracker))
+			return false;
+
+		if (has_align_tracker)
+		{
+			section->release_aligner = new GOrgueReleaseAlignTable();
+			if (!section->release_aligner->Load(cache))
+				return false;
+		}
+
+	}
+
+	return true;
+
 }
 
 bool GOSoundProvider::LoadCache(wxInputStream* cache)
 {
 
-	cache->Read(&m_attack, sizeof(m_attack));
-	if (cache->LastRead() != sizeof(m_attack))
+	cache->Read(&m_ScaleAmp, sizeof(m_ScaleAmp));
+	if (cache->LastRead() != sizeof(m_ScaleAmp))
+		return false;
+
+	cache->Read(&m_ScaleShift, sizeof(m_ScaleShift));
+	if (cache->LastRead() != sizeof(m_ScaleShift))
+		return false;
+
+	if (!LoadCacheAudioSection(cache, &m_Attack, false))
+		return false;
+
+	if (!LoadCacheAudioSection(cache, &m_Loop, false))
+		return false;
+
+	if (!LoadCacheAudioSection(cache, &m_Release, false))
+		return false;
+
+	return true;
+
+}
+
+bool GOSoundProvider::SaveCacheAudioSection
+	(wxOutputStream* cache, const AUDIO_SECTION* section, bool save_align_tracker)
+{
+
+	cache->Write(&section, sizeof(AUDIO_SECTION));
+	if (cache->LastWrite() != sizeof(AUDIO_SECTION))
+		return false;
+
+	cache->Write(section->data, section->alloc_size);
+	if (cache->LastWrite() != section->alloc_size)
+		return false;
+
+	if (save_align_tracker)
 	{
-		m_attack.data = NULL;
-		return false;
-	}
-	m_attack.data = (unsigned char*)malloc(m_attack.alloc_size);
-	if (m_attack.data == NULL)
-		throw (wxString)_("< out of memory allocating samples");
-	cache->Read(m_attack.data, m_attack.alloc_size);
-	if (cache->LastRead() != m_attack.alloc_size)
-		return false;
 
-	cache->Read(&m_loop, sizeof(m_loop));
-	if (cache->LastRead() != sizeof(m_loop))
-	{
-		m_loop.data = NULL;
-		return false;
-	}
-	m_loop.data = (unsigned char*)malloc(m_loop.alloc_size);
-	if (m_loop.data == NULL)
-		throw (wxString)_("< out of memory allocating samples");
-	cache->Read(m_loop.data, m_loop.alloc_size);
-	if (cache->LastRead() != m_loop.alloc_size)
-		return false;
-
-	cache->Read(&m_release, sizeof(m_release));
-	m_release.release_aligner = NULL;
-	m_release.data = NULL;
-	if (cache->LastRead() != sizeof(m_release))
-		return false;
-
-	m_release.data = (unsigned char*)malloc(m_release.alloc_size);
-	if (m_release.data == NULL)
-		throw (wxString)_("< out of memory allocating samples");
-	cache->Read(m_release.data, m_release.alloc_size);
-	if (cache->LastRead() != m_release.alloc_size)
-		return false;
-
-	cache->Read(&ra_amp, sizeof(ra_amp));
-	if (cache->LastRead() != sizeof(ra_amp))
-		return false;
-
-	cache->Read(&ra_shift, sizeof(ra_shift));
-	if (cache->LastRead() != sizeof(ra_shift))
-		return false;
-
-	bool release;
-	cache->Read(&release, sizeof(release));
-	if (cache->LastRead() != sizeof(release))
-		return false;
-
-	if (release)
-	{
-		m_release.release_aligner = new GOrgueReleaseAlignTable();
-		if (!m_release.release_aligner->Load(cache))
+		bool release = section->release_aligner != NULL;
+		cache->Write(&release, sizeof(release));
+		if (cache->LastWrite() != sizeof(release))
 			return false;
+
+		if (release)
+			if (!section->release_aligner->Save(cache))
+				return false;
+
 	}
 
 	return true;
@@ -118,43 +148,23 @@ bool GOSoundProvider::LoadCache(wxInputStream* cache)
 
 bool GOSoundProvider::SaveCache(wxOutputStream* cache)
 {
-	cache->Write(&m_attack, sizeof(m_attack));
-	if (cache->LastWrite() != sizeof(m_attack))
-		return false;
-	cache->Write(m_attack.data, m_attack.alloc_size);
-	if (cache->LastWrite() != m_attack.alloc_size)
+
+	cache->Write(&m_ScaleAmp, sizeof(m_ScaleAmp));
+	if (cache->LastWrite() != sizeof(m_ScaleAmp))
 		return false;
 
-	cache->Write(&m_loop, sizeof(m_loop));
-	if (cache->LastWrite() != sizeof(m_loop))
-		return false;
-	cache->Write(m_loop.data, m_loop.alloc_size);
-	if (cache->LastWrite() != m_loop.alloc_size)
+	cache->Write(&m_ScaleShift, sizeof(m_ScaleShift));
+	if (cache->LastWrite() != sizeof(m_ScaleShift))
 		return false;
 
-	cache->Write(&m_release, sizeof(m_release));
-	if (cache->LastWrite() != sizeof(m_release))
-		return false;
-	cache->Write(m_release.data, m_release.alloc_size);
-	if (cache->LastWrite() != m_release.alloc_size)
+	if (!SaveCacheAudioSection(cache, &m_Attack, false))
 		return false;
 
-	cache->Write(&ra_amp, sizeof(ra_amp));
-	if (cache->LastWrite() != sizeof(ra_amp))
+	if (!SaveCacheAudioSection(cache, &m_Loop, false))
 		return false;
 
-	cache->Write(&ra_shift, sizeof(ra_shift));
-	if (cache->LastWrite() != sizeof(ra_shift))
+	if (!SaveCacheAudioSection(cache, &m_Release, true))
 		return false;
-
-	bool release = m_release.release_aligner != NULL;
-	cache->Write(&release, sizeof(release));
-	if (cache->LastWrite() != sizeof(release))
-		return false;
-
-	if (release)
-		if (!m_release.release_aligner->Save(cache))
-			return false;
 
 	return true;
 
@@ -201,21 +211,21 @@ void GOSoundProvider::GetMaxAmplitudeAndDerivative
 void GOSoundProvider::ComputeReleaseAlignmentInfo()
 {
 
-	DELETE_AND_NULL(m_release.release_aligner);
+	DELETE_AND_NULL(m_Release.release_aligner);
 
 	/* Find the maximum amplitude and derivative of the waveform */
 	int phase_align_max_amplitude = 0;
 	int phase_align_max_derivative = 0;
-	GetMaxAmplitudeAndDerivative(m_attack,  phase_align_max_amplitude, phase_align_max_derivative);
-	GetMaxAmplitudeAndDerivative(m_loop,    phase_align_max_amplitude, phase_align_max_derivative);
-	GetMaxAmplitudeAndDerivative(m_release, phase_align_max_amplitude, phase_align_max_derivative);
+	GetMaxAmplitudeAndDerivative(m_Attack,  phase_align_max_amplitude, phase_align_max_derivative);
+	GetMaxAmplitudeAndDerivative(m_Loop,    phase_align_max_amplitude, phase_align_max_derivative);
+	GetMaxAmplitudeAndDerivative(m_Release, phase_align_max_amplitude, phase_align_max_derivative);
 
 	if ((phase_align_max_derivative != 0) && (phase_align_max_amplitude != 0))
 	{
 
-		m_release.release_aligner = new GOrgueReleaseAlignTable();
-		m_release.release_aligner->ComputeTable
-			(m_release
+		m_Release.release_aligner = new GOrgueReleaseAlignTable();
+		m_Release.release_aligner->ComputeTable
+			(m_Release
 			,phase_align_max_amplitude
 			,phase_align_max_derivative
 			,m_SampleRate
@@ -228,5 +238,5 @@ void GOSoundProvider::ComputeReleaseAlignmentInfo()
 
 int GOSoundProvider::IsOneshot() const
 {
-	return (m_loop.data == NULL);
+	return (m_Loop.data == NULL);
 }
