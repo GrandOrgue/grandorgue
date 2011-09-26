c8ef9bd376e312efa75df39252921285cc890bbc
Nick Appleton <nick@appletonaudio.com>
2011-09-26 09:40:38 +0000

    Add offload thread implementation (MK)
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@378 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOSoundThread.h
@@ -0,0 +1,51 @@
+/*
+ * GrandOrgue - free pipe organ simulator based on MyOrgan
+ *
+ * MyOrgan 1.0.6 Codebase - Copyright 2006 Milan Digital Audio LLC
+ * MyOrgan is a Trademark of Milan Digital Audio LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#ifndef GOSOUNDTHREAD_H_
+#define GOSOUNDTHREAD_H_
+
+#include <wx/wx.h>
+
+class GOSoundEngine;
+
+class GOSoundThread : public wxThread
+{
+private:
+	int m_MinSamplerID;
+	int m_MaxSamplerID;
+	GOSoundEngine* m_Engine;
+	unsigned m_FrameCount;
+	bool m_Stop;
+
+	wxMutex m_Mutex;
+	wxCondition m_Condition;
+
+	void* Entry();
+
+public:
+	GOSoundThread(GOSoundEngine* engine, int min_sampler_id, int max_sampler_id, unsigned n_frames);
+
+	void Delete();
+	void Wakeup();
+};
+
+#endif
