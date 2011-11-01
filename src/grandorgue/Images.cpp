bc0cbb3c709bf2e4b631ca41f5c809d6ed9daa13
nappleton <nappleton@69509848-2a73-49d5-a893-c4d8c934025a>
2011-11-01 11:05:17 +0000

    Correct image list broken by my last patch (MK)
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@462 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/Images.cpp
+++ src/grandorgue/Images.cpp
@@ -26,7 +26,7 @@ static const GetImage_Func ImageLoader_Wood[] = {
 	GetImage_Wood01, GetImage_Wood01_r, GetImage_Wood03, GetImage_Wood03_r, GetImage_Wood05, GetImage_Wood05_r, 
 	GetImage_Wood07, GetImage_Wood07_r, GetImage_Wood09, GetImage_Wood09_r, GetImage_Wood11, GetImage_Wood11_r, 
 	GetImage_Wood13, GetImage_Wood13_r, GetImage_Wood15, GetImage_Wood15_r, GetImage_Wood17, GetImage_Wood17_r, 
-	GetImage_Wood19, GetImage_Wood19_r, GetImage_Wood21, GetImage_Wood21_r, GetImage_Wood21, GetImage_Wood21_r, 
+	GetImage_Wood19, GetImage_Wood19_r, GetImage_Wood21, GetImage_Wood21_r,
 	GetImage_Wood23, GetImage_Wood23_r, GetImage_Wood25, GetImage_Wood25_r, GetImage_Wood27, GetImage_Wood27_r, 
 	GetImage_Wood29, GetImage_Wood29_r, GetImage_Wood31, GetImage_Wood31_r, GetImage_Wood33, GetImage_Wood33_r, 
 	GetImage_Wood35, GetImage_Wood35_r, GetImage_Wood37, GetImage_Wood37_r, GetImage_Wood39, GetImage_Wood39_r, 
