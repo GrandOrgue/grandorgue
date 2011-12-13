e0e53bb64c93df0c56ce78b3b0db8ff6b320f7c4
Nick Appleton <nick@appletonaudio.com>
2011-12-13 11:14:57 +0000

    Includes to fix build problems
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@623 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/portaudio/hostapi/wasapi/mingw-include/functiondiscoverykeys.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#if __GNUC__ >=3
+#pragma GCC system_header
+#endif
+
+#ifndef DEFINE_API_PKEY
+#include <propkey.h>
+#endif
+
+DEFINE_PROPERTYKEY(PKEY_Device_FriendlyName, 0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0, 14);
+
---
+++ src/portaudio/hostapi/wasapi/mingw-include/sal.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if __GNUC__ >=3
+#pragma GCC system_header
+#endif
+
+#define DEFINE_ENUM_FLAG_OPERATORS(ENUMTYPE)                                                                                                                                                                                                                                   
+#define DECLSPEC_HIDDEN  
+#define eConnTypeRJ11Modem eConnTypeRj11Modem
