3616723ee6c3db5e1d28c25d57ffe3e7a1af7276
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2011-12-18 21:29:43 +0000

    Add temperament class
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@643 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueTemperament.cpp
@@ -0,0 +1,76 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2011 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * MA 02111-1307, USA.
+ */
+
+#include "GOrgueTemperament.h"
+
+GOrgueTemperament::GOrgueTemperament(wxString name) :
+	m_Name(name)
+{
+}
+
+GOrgueTemperament::~GOrgueTemperament()
+{
+}
+
+float GOrgueTemperament::GetOffset(unsigned midi_number, unsigned wav_midi_number, float wav_pitch_fract, float harmonic_number, float default_tuning) const
+{
+	return 0;
+}
+
+wxString GOrgueTemperament::GetName() const
+{
+	return m_Name;
+}
+
+std::vector<wxString> GOrgueTemperament::GetNames()
+{
+	InitTemperaments();
+
+	std::vector<wxString> names;
+	for(unsigned i = 0; i < m_Temperaments.size(); i++)
+		names.push_back(m_Temperaments[i]->GetName());
+
+	return names;
+}
+
+const GOrgueTemperament& GOrgueTemperament::GetTemperament(wxString Name)
+{
+	InitTemperaments();
+
+	for(unsigned i = 0; i < m_Temperaments.size(); i++)
+		if (Name == m_Temperaments[i]->GetName())
+			return *m_Temperaments[i];
+
+	/* else return original temperament */
+	return *m_Temperaments[0];
+}
+
+ptr_vector<GOrgueTemperament> GOrgueTemperament::m_Temperaments;
+
+void GOrgueTemperament::InitTemperaments()
+{
+	if (m_Temperaments.size())
+		return;
+
+	m_Temperaments.push_back(new GOrgueTemperament(wxTRANSLATE("Original temperament")));
+}
+
---
+++ src/grandorgue/GOrgueTemperament.h
@@ -0,0 +1,49 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2011 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * MA 02111-1307, USA.
+ */
+
+#ifndef GORGUETEMPERAMENT_H
+#define GORGUETEMPERAMENT_H
+
+#include <wx/wx.h>
+#include "ptrvector.h"
+
+class GOrgueTemperament
+{
+	DECLARE_NO_COPY_CLASS(GOrgueTemperament)
+protected:
+	wxString m_Name;
+
+	static ptr_vector<GOrgueTemperament> m_Temperaments;
+	static void InitTemperaments();
+
+public:
+	GOrgueTemperament(wxString name);
+	virtual ~GOrgueTemperament();
+
+	virtual float GetOffset(unsigned midi_number, unsigned wav_midi_number, float wav_pitch_fract, float harmonic_number, float default_tuning) const;
+	wxString GetName() const;
+
+	static const GOrgueTemperament& GetTemperament(wxString Name);
+	static std::vector<wxString> GetNames();
+};
+
+#endif
