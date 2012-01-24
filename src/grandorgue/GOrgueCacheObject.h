4a67369341c7555c91c4e740573140bf76498910
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2012-01-24 08:15:28 +0000

    Add missing file
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@754 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueCacheObject.h
@@ -0,0 +1,42 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2012 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * MA 02111-1307, USA.
+ */
+
+#ifndef GORGUECACHEOBJECT_H
+#define GORGUECACHEOBJECT_H
+
+#include <wx/wx.h>
+#include "contrib/sha1.h"
+class GOrgueCache;
+class GOrgueCacheWriter;
+
+class GOrgueCacheObject
+{
+public:
+	virtual void Initialize() = 0;
+	virtual void LoadData() = 0;
+	virtual bool LoadCache(GOrgueCache& cache) = 0;
+	virtual bool SaveCache(GOrgueCacheWriter& cache) = 0;
+	virtual void UpdateHash(SHA_CTX& ctx) = 0;
+	virtual wxString GetLoadTitle() = 0;
+};
+
+#endif
