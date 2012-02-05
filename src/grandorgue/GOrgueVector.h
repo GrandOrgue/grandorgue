c0dc1c6252f966b613bdfa73cd245184c22c2d0e
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2012-02-05 17:30:46 +0000

    Start using GCC vector extensions
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@786 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueVector.h
@@ -0,0 +1,43 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2012 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * MA 02111-1307, USA.
+ */
+
+#ifndef GORGUEVECTOR_H
+#define GORGUEVECTOR_H
+
+#ifdef __GNUC__
+#define GO_VECTOR_SIZE 4
+typedef float GOVector __attribute__ ((vector_size (16)));
+static inline GOVector GOConstVector(float a)
+{
+	GOVector x = { (a), (a), (a), (a) };
+	return x;
+}
+#else
+#define GO_VECTOR_SIZE 1
+typedef float GOVector;
+static inline GOVector GOConstVector(float a)
+{
+	return a;
+}
+#endif
+
+#endif
