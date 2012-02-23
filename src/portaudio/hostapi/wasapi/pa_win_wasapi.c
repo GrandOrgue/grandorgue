b5e8949345da17f47c13cd3d8eededd58e10f994
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2012-02-23 07:57:30 +0000

    Portaudio upstream update
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@836 69509848-2a73-49d5-a893-c4d8c934025a

--- src/portaudio/hostapi/wasapi/pa_win_wasapi.c
+++ src/portaudio/hostapi/wasapi/pa_win_wasapi.c
@@ -1248,7 +1248,7 @@ PaError PaWasapi_Initialize( PaUtilHostApiRepresentation **hostApi, PaHostApiInd
                         goto error;
                     }
 					if (value.pwszVal)
-						wcstombs(deviceName, value.pwszVal, MAX_STR_LEN-1);
+						WideCharToMultiByte(CP_UTF8, 0, value.pwszVal, (int)wcslen(value.pwszVal), deviceName, MAX_STR_LEN-1, 0, 0);
 					else
 						_snprintf(deviceName, MAX_STR_LEN-1, "baddev%d", i);
                     deviceInfo->name = deviceName;
@@ -2214,11 +2214,15 @@ static HRESULT CreateAudioClient(PaWasapiStream *pStream, PaWasapiSubStream *pSu
 	if (framesPerLatency == 0)
 		framesPerLatency = MakeFramesFromHns(pInfo->DefaultDevicePeriod, pSub->wavex.Format.nSamplesPerSec);
 
-	//! Exclusive Input stream renders data in 6 packets, we must set then the size of
-	//! single packet, total buffer size, e.g. required latency will be PacketSize * 6
+	// Exclusive Input stream renders data in 6 packets, we must set then the size of
+	// single packet, total buffer size, e.g. required latency will be PacketSize * 6
 	if (!output && (pSub->shareMode == AUDCLNT_SHAREMODE_EXCLUSIVE))
 	{
-		framesPerLatency /= WASAPI_PACKETS_PER_INPUT_BUFFER;
+		// Do it only for Polling mode
+		if ((pSub->streamFlags & AUDCLNT_STREAMFLAGS_EVENTCALLBACK) == 0)
+		{
+			framesPerLatency /= WASAPI_PACKETS_PER_INPUT_BUFFER;
+		}
 	}
 
 	// Calculate aligned period
@@ -3463,7 +3467,7 @@ static PaError ReadStream( PaStream* s, void *_buffer, unsigned long frames )
 
 		// Limit desired to amount of requested frames
 		desired = available;
-		if (desired > frames)
+		if ((UINT32)desired > frames)
 			desired = frames;
 		
 		// Get pointers to read regions
@@ -4800,13 +4804,15 @@ PA_THREAD_FUNC ProcThreadPoll(void *param)
 					// output
 					if (stream->bufferMode == paUtilFixedHostBufferSize)
 					{
-						if (frames >= stream->out.framesPerBuffer)
+						while (frames >= stream->out.framesPerBuffer)
 						{
 							if ((hr = ProcessOutputBuffer(stream, processor, stream->out.framesPerBuffer)) != S_OK)
 							{
 								LogHostError(hr);
 								goto thread_error;
 							}
+
+							frames -= stream->out.framesPerBuffer;
 						}
 					}
 					else
