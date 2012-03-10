c5e490eef5f912f920795394e09338c66fa3ad90
Martin Koegler <martin.koegler@chello.at>
2012-03-10 19:22:03 +0000

    Make splash screen text variable
    ASIO message is fixed included to avoid accidental bugs because of
    conditional compilation
    
    
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@866 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/SplashScreen.cpp
+++ src/grandorgue/SplashScreen.cpp
@@ -22,6 +22,7 @@
 
 #include <wx/splash.h>
 #include "SplashScreen.h"
+#include "GrandOrgueDef.h"
 
 #define wxSPLASH_TIMER_ID 9999
 
@@ -30,13 +31,42 @@ BEGIN_EVENT_TABLE(wxSplashScreenModal, wxDialog)
     EVT_CLOSE(wxSplashScreenModal::OnCloseWindow)
 END_EVENT_TABLE()
 
-wxSplashScreenModal::wxSplashScreenModal(const wxBitmap& bitmap, long splashStyle, int milliseconds, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style):
+wxSplashScreenModal::wxSplashScreenModal(const wxBitmap& bmp, long splashStyle, int milliseconds, wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style):
     wxDialog(parent, id, wxEmptyString, wxPoint(0,0), wxSize(100, 100), style)
 {
+	wxBitmap bitmap = bmp;
+	m_window = NULL;
+	m_splashStyle = splashStyle;
+	m_milliseconds = milliseconds;
 
-    m_window = NULL;
-    m_splashStyle = splashStyle;
-    m_milliseconds = milliseconds;
+	wxMemoryDC dc;
+	wxFont font;
+	dc.SelectObject(bitmap);
+	
+	font = *wxNORMAL_FONT;
+	font.SetPointSize(14);
+	font.SetWeight(wxFONTWEIGHT_BOLD);
+	dc.SetFont(font);
+	dc.DrawLabel(wxT(APP_TITLE), wxRect(60, 0, 370, 22), wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
+
+	font = *wxNORMAL_FONT;
+	font.SetWeight(wxFONTWEIGHT_BOLD);
+	font.SetPointSize(10);
+	dc.SetFont(font);
+	dc.DrawLabel(_("virtual pipe organ simulator"), wxRect(60, 22, 370, 20), wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
+
+	font = *wxNORMAL_FONT;
+	font.SetPointSize(10);
+	font.SetWeight(wxFONTWEIGHT_NORMAL);
+	dc.SetFont(font);
+	dc.DrawLabel(_("licensed under the GNU GPLv2 (or later)"), wxRect(60, 42, 370, 20), wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
+
+	font = *wxNORMAL_FONT;
+	font.SetWeight(wxFONTWEIGHT_NORMAL);
+	font.SetPointSize(7);
+	dc.SetFont(font);
+	wxString msg = _("Copyright 2006 Milan Digital Audio LLC\nCopyright 2009-2012 GrandOrgue contributors\n\nThis software comes with no warranty.\n\nASIO Interface Technology by Steinberg Media Technologies GmbH,\nby use of the Steinberg ASIO SDK, Version 2.2.\nASIO is a trademark and software of Steinberg Media Technologies GmbH.");
+	dc.DrawLabel(msg, wxRect(60, 62, 370, 100), wxALIGN_CENTER_HORIZONTAL | wxALIGN_CENTER_VERTICAL);
 
     m_window = new wxSplashScreenWindow(bitmap, this, wxID_ANY, pos, size, wxNO_BORDER);
 
