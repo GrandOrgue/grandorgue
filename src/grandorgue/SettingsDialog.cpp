e2a5f6276b8471fe56decc9b867bc68be488a3e3
Martin Koegler <martin.koegler@chello.at>
2012-03-10 19:23:42 +0000

    Use splitted panels
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@868 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/SettingsDialog.cpp
+++ src/grandorgue/SettingsDialog.cpp
@@ -20,16 +20,10 @@
  * MA 02111-1307, USA.
  */
 
-#include <wx/filepicker.h>
 #include <wx/spinctrl.h>
-#include <wx/tglbtn.h>
 #include <wx/bookctrl.h>
 #include <wx/numdlg.h>
-#include <wx/html/helpctrl.h>
-#include <wx/statline.h>
-#include <wx/listctrl.h>
 
-#include "MIDIListenDialog.h"
 #include "SettingsDialog.h"
 #include "GrandOrgueID.h"
 #include "GOrgueEvent.h"
@@ -37,6 +31,9 @@
 #include "GOrgueSettings.h"
 #include "GOrgueSound.h"
 #include "GOrgueRtHelpers.h"
+#include "SettingsMidiMessage.h"
+#include "SettingsOption.h"
+#include "SettingsOrgan.h"
 
 IMPLEMENT_CLASS(SettingsDialog, wxPropertySheetDialog)
 
@@ -48,30 +45,10 @@ BEGIN_EVENT_TABLE(SettingsDialog, wxPropertySheetDialog)
 	EVT_CHOICE(ID_SOUND_DEVICE, SettingsDialog::OnDevicesSoundChoice)
 	EVT_CHOICE(ID_MONO_STEREO, SettingsDialog::OnChanged)
 	EVT_CHOICE(ID_SAMPLE_RATE, SettingsDialog::OnChanged)
-	EVT_CHOICE(ID_CONCURRENCY, SettingsDialog::OnChanged)
-	EVT_CHOICE(ID_RELEASE_CONCURRENCY, SettingsDialog::OnChanged)
-	EVT_CHOICE(ID_WAVE_FORMAT, SettingsDialog::OnChanged)
 	EVT_CHOICE(ID_LOOP_LOAD, SettingsDialog::OnChanged)
 	EVT_CHOICE(ID_ATTACK_LOAD, SettingsDialog::OnChanged)
 	EVT_CHOICE(ID_RELEASE_LOAD, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_ENHANCE_SQUASH, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_ENHANCE_MANAGE_POLYPHONY, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_COMPRESS_CACHE, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_ENHANCE_SCALE_RELEASE, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_ENHANCE_RANDOMIZE, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_SETTINGS_DIR, SettingsDialog::OnChanged)
-	EVT_CHECKBOX(ID_CACHE_DIR, SettingsDialog::OnChanged)
 	EVT_CHOICE(ID_INTERPOLATION, SettingsDialog::OnChanged)
-
-	EVT_LIST_ITEM_SELECTED(ID_MIDI_EVENTS, SettingsDialog::OnEventListClick)
-	EVT_LIST_ITEM_ACTIVATED(ID_MIDI_EVENTS, SettingsDialog::OnEventListDoubleClick)
-	EVT_BUTTON(ID_EVENT_PROPERTIES, SettingsDialog::OnEventListDoubleClickC)
-
-    EVT_LIST_ITEM_SELECTED(ID_ORGAN_EVENTS, SettingsDialog::OnOrganEventListClick)
-    EVT_LIST_ITEM_ACTIVATED(ID_MIDI_EVENTS, SettingsDialog::OnOrganEventListDoubleClick)
-    EVT_BUTTON(ID_EVENT_ADD, SettingsDialog::OnAddOrgan)
-    EVT_BUTTON(ID_EVENT_DEL, SettingsDialog::OnDelOrgan)
-    EVT_BUTTON(ID_EVENT_ORGANPROPERTIES, SettingsDialog::OnOrganProperties)
 
 	EVT_BUTTON(wxID_APPLY, SettingsDialog::OnApply)
 	EVT_BUTTON(wxID_OK, SettingsDialog::OnOK)
@@ -111,7 +88,6 @@ SettingsDialog::SettingsDialog(wxWindow* win, GOrgueSound& sound) :
 	m_Settings(sound.GetSettings())
 {
 	b_stereo = m_Settings.GetLoadInStereo();
-	b_squash = m_Settings.GetLosslessCompression();
 	m_OldBitsPerSample = m_Settings.GetBitsPerSample();
 	m_OldLoopLoad = m_Settings.GetLoopLoad();
 	m_OldAttackLoad = m_Settings.GetAttackLoad();
@@ -123,34 +99,23 @@ SettingsDialog::SettingsDialog(wxWindow* win, GOrgueSound& sound) :
 	wxBookCtrlBase* notebook = GetBookCtrl();
 
 	wxPanel* devices  = CreateDevicesPage(notebook);
-	wxPanel* messages = CreateMessagesPage(notebook);
-	wxPanel* organs = CreateOrganPage(notebook);
-	wxPanel* options = CreateOptionsPage(notebook);
+	m_OptionsPage = new SettingsOption(m_Settings, notebook);
+	m_OrganPage = new SettingsOrgan(m_Settings, notebook);
+	m_MidiMessagePage = new SettingsMidiMessage(m_Settings, notebook);
 
 	notebook->AddPage(devices,  _("Devices"));
-	notebook->AddPage(options,  _("Options"));
-	notebook->AddPage(messages, _("MIDI Messages"));
-	notebook->AddPage(organs, _("Organs"));
-
-	//this->FindWindowById(wxID_APPLY, this)->Disable();
-
-	//LayoutDialog();
-
-	page2list->SetColumnWidth(0, wxLIST_AUTOSIZE);
-	page2list->SetColumnWidth(0, page2list->GetColumnWidth(0) + 16);
-	page2list->SetColumnWidth(2, 62);
-	page2list->SetColumnWidth(3, 100);
-
-	page2list->SetColumnWidth(1, page2list->GetClientSize().GetWidth() - page2list->GetColumnWidth(0) - 108);
+	notebook->AddPage(m_OptionsPage,  _("Options"));
+	notebook->AddPage(m_MidiMessagePage, _("Initial MIDI Configuration"));
+	notebook->AddPage(m_OrganPage, _("Organs"));
 }
 
 SettingsDialog::~SettingsDialog()
 {
 	if ((b_stereo != m_Settings.GetLoadInStereo() || 
-	     b_squash != m_Settings.GetLosslessCompression() ||
 	     m_OldLoopLoad != m_Settings.GetLoopLoad() ||
 	     m_OldAttackLoad != m_Settings.GetAttackLoad() ||
 	     m_OldReleaseLoad != m_Settings.GetReleaseLoad() ||
+	     m_OptionsPage->NeedReload() ||
 	     m_OldBitsPerSample != m_Settings.GetBitsPerSample()) &&
 	    m_Sound.GetOrganFile() != NULL)
 	{
@@ -247,7 +212,7 @@ wxPanel* SettingsDialog::CreateDevicesPage(wxWindow* parent)
 	for (unsigned i = 0; i < 5; i++)
 		choices.push_back(wxString::Format(wxT("%d bits"), 8 + i * 4));
 	grid->Add(new wxStaticText(panel, wxID_ANY, _("Sample size:")), 0, wxALL | wxALIGN_CENTER_VERTICAL);
-	grid->Add(c_BitsPerSample = new wxChoice(panel, ID_CONCURRENCY, wxDefaultPosition, wxDefaultSize, choices), 0, wxALL);
+	grid->Add(c_BitsPerSample = new wxChoice(panel, ID_BITS_PER_SAMPLE, wxDefaultPosition, wxDefaultSize, choices), 0, wxALL);
 
 	choices.clear();
 	choices.push_back(_("First loop"));
@@ -293,337 +258,6 @@ wxPanel* SettingsDialog::CreateDevicesPage(wxWindow* parent)
 	return panel;
 }
 
-wxPanel* SettingsDialog::CreateOptionsPage(wxWindow* parent)
-{
-	wxArrayString choices;
-
-	wxPanel* panel = new wxPanel(parent, wxID_ANY);
-	wxBoxSizer* topSizer = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* item0 = new wxBoxSizer(wxHORIZONTAL);
-
-	wxBoxSizer* item9 = new wxBoxSizer(wxVERTICAL);
-	item0->Add(item9, 0, wxEXPAND | wxALL, 0);
-
-	wxBoxSizer* item6 = new wxStaticBoxSizer(wxVERTICAL, panel, _("&Enhancements"));
-	item9->Add(item6, 0, wxEXPAND | wxALL, 5);
-	item6->Add(c_squash = new wxCheckBox(panel, ID_ENHANCE_SQUASH,           _("Lossless compression")          ), 0, wxEXPAND | wxALL, 5);
-	item6->Add(c_limit  = new wxCheckBox(panel, ID_ENHANCE_MANAGE_POLYPHONY, _("Active polyphony management")   ), 0, wxEXPAND | wxALL, 5);
-	item6->Add(c_CompressCache  = new wxCheckBox(panel, ID_COMPRESS_CACHE,    _("Compress Cache")), 0, wxEXPAND | wxALL, 5);
-	item6->Add(c_scale  = new wxCheckBox(panel, ID_ENHANCE_SCALE_RELEASE,    _("Release sample scaling"        )), 0, wxEXPAND | wxALL, 5);
-	item6->Add(c_random = new wxCheckBox(panel, ID_ENHANCE_RANDOMIZE,        _("Randomize pipe speaking"       )), 0, wxEXPAND | wxALL, 5);
-	if (m_Settings.GetLosslessCompression())
-		c_squash->SetValue(true);
-	if (m_Settings.GetManagePolyphony())
-		c_limit ->SetValue(true);
-	if (m_Settings.GetCompressCache())
-		c_CompressCache ->SetValue(true);
-	if (m_Settings.GetScaleRelease())
-		c_scale ->SetValue(true);
-	if (m_Settings.GetRandomizeSpeaking())
-		c_random->SetValue(true);
-
-
-	wxFlexGridSizer* grid = new wxFlexGridSizer(4, 2, 5, 5);
-	item6 = new wxStaticBoxSizer(wxVERTICAL, panel, _("&Sound Engine"));
-	item9->Add(item6, 0, wxEXPAND | wxALL, 5);
-
-	choices.clear();
-	for (unsigned i = 0; i < 15; i++)
-		choices.push_back(wxString::Format(wxT("%d"), i));
-	grid->Add(new wxStaticText(panel, wxID_ANY, _("Concurreny Level:")), 0, wxALL | wxALIGN_CENTER_VERTICAL);
-	grid->Add(c_Concurrency = new wxChoice(panel, ID_CONCURRENCY, wxDefaultPosition, wxDefaultSize, choices), 0, wxALL);
-
-	choices.clear();
-	for (unsigned i = 1; i < 8; i++)
-		choices.push_back(wxString::Format(wxT("%d"), i));
-	grid->Add(new wxStaticText(panel, wxID_ANY, _("Release Concurreny Level:")), 0, wxALL | wxALIGN_CENTER_VERTICAL);
-	grid->Add(c_ReleaseConcurrency = new wxChoice(panel, ID_CONCURRENCY, wxDefaultPosition, wxDefaultSize, choices), 0, wxALL);
-
-	choices.clear();
-	choices.push_back(_("8 Bit PCM"));
-	choices.push_back(_("16 Bit PCM"));
-	choices.push_back(_("24 Bit PCM"));
-	choices.push_back(_("IEEE Float"));
-	grid->Add(new wxStaticText(panel, wxID_ANY, _("Recorder WAV Format:")), 0, wxALL | wxALIGN_CENTER_VERTICAL);
-	grid->Add(c_WaveFormat = new wxChoice(panel, ID_WAVE_FORMAT, wxDefaultPosition, wxDefaultSize, choices), 0, wxALL);
-	item6->Add(grid, 0, wxEXPAND | wxALL, 5);
-
-	c_Concurrency->Select(m_Settings.GetConcurrency());
-	c_ReleaseConcurrency->Select(m_Settings.GetReleaseConcurrency() - 1);
-	c_WaveFormat->Select(m_Settings.GetWaveFormatBytesPerSample() - 1);
-
-	grid = new wxFlexGridSizer(4, 1, 5, 5);
-	item6 = new wxStaticBoxSizer(wxVERTICAL, panel, _("&Paths"));
-	item9->Add(item6, 0, wxEXPAND | wxALL, 5);
-
-	grid->Add(new wxStaticText(panel, wxID_ANY, _("Settings store:")), 0, wxALL | wxALIGN_CENTER_VERTICAL);
-	grid->Add(c_SettingsPath = new wxDirPickerCtrl(panel, ID_SETTINGS_DIR, wxEmptyString, _("Select directory for settings store"), wxDefaultPosition, wxDefaultSize, 
-						       wxDIRP_DEFAULT_STYLE | wxDIRP_DIR_MUST_EXIST), 0, wxALL);
-
-	grid->Add(new wxStaticText(panel, wxID_ANY, _("Cache store:")), 0, wxALL | wxALIGN_CENTER_VERTICAL);
-	grid->Add(c_CachePath = new wxDirPickerCtrl(panel, ID_SETTINGS_DIR, wxEmptyString, _("Select directory for cache store"), wxDefaultPosition, wxDefaultSize, 
-						    wxDIRP_DEFAULT_STYLE | wxDIRP_DIR_MUST_EXIST), 0, wxALL);
-	item6->Add(grid, 0, wxEXPAND | wxALL, 5);
-
-	c_SettingsPath->SetPath(m_Settings.GetUserSettingPath());
-	c_CachePath->SetPath(m_Settings.GetUserCachePath());
-
-	topSizer->Add(item0, 1, wxEXPAND | wxALIGN_CENTER | wxALL, 5);
-	topSizer->AddSpacer(5);
-	panel->SetSizer(topSizer);
-	topSizer->Fit(panel);
-	return panel;
-}
-
-wxPanel* SettingsDialog::CreateMessagesPage(wxWindow* parent)
-{
-	wxPanel* panel = new wxPanel(parent, wxID_ANY);
-	wxBoxSizer* topSizer = new wxBoxSizer(wxVERTICAL);
-	topSizer->AddSpacer(5);
-
-	page2list = new wxListView(panel, ID_MIDI_EVENTS, wxDefaultPosition, wxDefaultSize, wxLC_REPORT | wxLC_SINGLE_SEL | wxLC_HRULES | wxLC_VRULES);
-	page2list->InsertColumn(0, _("Action"));
-	page2list->InsertColumn(1, _("Event"));
-	page2list->InsertColumn(2, _("Channel"));
-	page2list->InsertColumn(3, _("Data/Offset"));
-	topSizer->Add(page2list, 1, wxEXPAND | wxALL, 5);
-	page2button = new wxButton(panel, ID_EVENT_PROPERTIES, _("P&roperties..."));
-	page2button->Disable();
-	topSizer->Add(page2button, 0, wxALIGN_RIGHT | wxALL, 5);
-
-	for (unsigned i = 0; i < GetEventCount(); i++)
-	{
-		page2list->InsertItem(i, GetEventName(i));
-		UpdateMessages(i);
-	}
-
-	topSizer->AddSpacer(5);
-	panel->SetSizer(topSizer);
-	topSizer->Fit(panel);
-
-	return panel;
-}
-
-wxPanel* SettingsDialog::CreateOrganPage(wxWindow* parent)
-{
-	wxPanel* panel = new wxPanel(parent, wxID_ANY);
-	wxBoxSizer* topSizer = new wxBoxSizer(wxVERTICAL);
-	topSizer->AddSpacer(5);
-
-	organlist = new wxListView(panel, ID_ORGAN_EVENTS, wxDefaultPosition, wxDefaultSize, wxLC_REPORT | wxLC_SINGLE_SEL | wxLC_HRULES | wxLC_VRULES);
-	organlist->InsertColumn(0, _("Organ"));
-	organlist->InsertColumn(1, _("Event"));
-	organlist->InsertColumn(2, _("Channel"));
-	organlist->InsertColumn(3, _("Data"));
-	topSizer->Add(organlist, 1, wxEXPAND | wxALL, 5);
-
-	wxBoxSizer* buttonSizer =  new wxBoxSizer(wxHORIZONTAL);
-	buttonSizer->AddSpacer(5);
-	propButton = new wxButton(panel, ID_EVENT_ORGANPROPERTIES, _("P&roperties..."));
-	propButton->Disable();
-	addButton = new wxButton(panel, ID_EVENT_ADD, _("&Add"));
-	delButton = new wxButton(panel, ID_EVENT_DEL, _("&Delete"));
-	delButton->Disable();
-
-	buttonSizer->Add(propButton, 0, wxALIGN_RIGHT | wxALL, 5);
-	buttonSizer->Add(addButton, 0, wxALIGN_LEFT | wxALL, 5);
-	buttonSizer->Add(delButton, 0, wxALIGN_LEFT | wxALL, 5);
-	topSizer->Add(buttonSizer, 0, wxALL, 5);
-
-	std::map<long, wxString>::const_iterator it = m_Settings.GetOrganList().begin();
-	for(; it != m_Settings.GetOrganList().end(); it++)
-	{
-		unsigned i = organlist->GetItemCount();
-		organlist->InsertItem(i, it->second);
-		organlist->SetItemData(i, it->first);
-		UpdateOrganMessages(i);
-	}
-	if (organlist->GetItemCount())
-	{
-		organlist->SetColumnWidth(0, wxLIST_AUTOSIZE);
-		organlist->SetColumnWidth(1, wxLIST_AUTOSIZE);
-		organlist->SetColumnWidth(2, wxLIST_AUTOSIZE_USEHEADER);
-		organlist->SetColumnWidth(3, wxLIST_AUTOSIZE_USEHEADER);
-	}
-
-	topSizer->AddSpacer(5);
-	panel->SetSizer(topSizer);
-	topSizer->Fit(panel);
-	return panel;
-}
-
-
-void SettingsDialog::OnOrganEventListClick(wxListEvent& event)
-{
-	delButton->Enable();
-	propButton->Enable();
-}
-
-void SettingsDialog::OnOrganEventListDoubleClick(wxListEvent& event)
-{
-}
-
-void SettingsDialog::OnAddOrgan(wxCommandEvent& event)
-{
-	//ask for .organ file
-	wxFileDialog dlg(this, _("Choose a file"), wxT(""), wxT(""), _("*.organ"), wxFD_FILE_MUST_EXIST | wxFD_OPEN);
-	if (dlg.ShowModal()==wxID_OK)
-	{
-		organlist->InsertItem(organlist->GetItemCount(), dlg.GetPath());
-		organlist->SetColumnWidth(0, wxLIST_AUTOSIZE);
-	}
-}
-
-void SettingsDialog::OnDelOrgan(wxCommandEvent& event)
-{
-	int index=organlist->GetFirstSelected();
-	organlist->DeleteItem(index);
-}
-
-void SettingsDialog::OnOrganProperties(wxCommandEvent& event)
-{
-	int index = organlist->GetFirstSelected();
-
-	MIDIListenDialog dlg
-		(this
-		,organlist->GetItemText(index)
-		,MIDIListenDialog::LSTN_SETTINGSDLG_MEMORY_OR_ORGAN
-		,organlist->GetItemData(index)
-		);
-
-	if (dlg.ShowModal() == wxID_OK)
-	{
-		organlist->SetItemData(index, dlg.GetEvent());
-		UpdateOrganMessages(index);
-		organlist->SetColumnWidth(1, wxLIST_AUTOSIZE);
-		organlist->SetColumnWidth(2, wxLIST_AUTOSIZE_USEHEADER);
-		organlist->SetColumnWidth(3, wxLIST_AUTOSIZE_USEHEADER);
-	}
-
-}
-
-unsigned SettingsDialog::GetEventCount()
-{
-	return m_Settings.GetSetterCount() + m_Settings.GetEnclosureCount() +
-		m_Settings.GetManualCount() + 1;
-}
-
-MIDIListenDialog::LISTEN_DIALOG_TYPE SettingsDialog::GetEventType(unsigned index)
-{
-	if (index < m_Settings.GetSetterCount())
-		return MIDIListenDialog::LSTN_SETTINGSDLG_MEMORY_OR_ORGAN;
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-		return MIDIListenDialog::LSTN_ENCLOSURE;
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-		return MIDIListenDialog::LSTN_MANUAL;
-	index -= m_Settings.GetManualCount();
-
-	return MIDIListenDialog::LSTN_SETTINGSDLG_STOP_CHANGE;
-}
-
-wxString SettingsDialog::GetEventName(unsigned index)
-{
-	if (index < m_Settings.GetSetterCount())
-		return m_Settings.GetSetterTitle(index);
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-		return m_Settings.GetEnclosureTitle(index);
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-		return m_Settings.GetManualTitle(index);
-	index -= m_Settings.GetManualCount();
-
-	return m_Settings.GetStopChangeTitle();
-}
-
-int SettingsDialog::GetEventData(unsigned index)
-{
-	if (index < m_Settings.GetSetterCount())
-		return m_Settings.GetSetterEvent(index);
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-		return m_Settings.GetEnclosureEvent(index);
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-		return m_Settings.GetManualEvent(index);
-	index -= m_Settings.GetManualCount();
-
-	return m_Settings.GetStopChangeEvent();
-}
-
-void SettingsDialog::SetEventData(unsigned index, int event)
-{
-	if (index < m_Settings.GetSetterCount())
-	{
-		m_Settings.SetSetterEvent(index, event);
-		return;
-	}
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-	{
-		m_Settings.SetEnclosureEvent(index, event);
-		return;
-	}
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-	{
-		m_Settings.SetManualEvent(index, event);
-		return;
-	}
-	index -= m_Settings.GetManualCount();
-
-	m_Settings.SetStopChangeEvent(event);
-}
-
-
-void SettingsDialog::UpdateMessages(unsigned i)
-{
-	MIDIListenDialog::LISTEN_DIALOG_TYPE type = GetEventType(i);
-	int j = GetEventData(i);
-	page2list->SetItem(i, 1, MIDIListenDialog::GetEventTitle(j, type));
-	page2list->SetItem(i, 2, MIDIListenDialog::GetEventChannelString(j));
-
-	if (type != MIDIListenDialog::LSTN_SETTINGSDLG_STOP_CHANGE && j)
-	{
-		int offset = (j & 0xF000) == 0xC000 ? 1 : 0;
-		wxString temp;
-		if (type == 1 ) // when type == 1 (keyboards) data contained in lower byte correspond to note offset
-		{
-			if ((j&0xFF)>0x7F) offset = -140; // negative numbers are coded as 128=-12 140=0
-		}
-		temp << ((j & 0xFF) + offset );
-		page2list->SetItem(i, 3, temp);
-	}
-
-	page2list->SetItemData(i, j);
-
-}
-
-void SettingsDialog::UpdateOrganMessages(int i)
-{
-
-	int j = organlist->GetItemData(i);
-	organlist->SetItem(i, 1, MIDIListenDialog::GetEventTitle(j, MIDIListenDialog::LSTN_SETTINGSDLG_MEMORY_OR_ORGAN));
-	organlist->SetItem(i, 2, MIDIListenDialog::GetEventChannelString(j));
-	// data
-	wxString temp;
-	int offset = (j & 0xF000) == 0xC000 ? 1 : 0;
-	temp << ((j & 0x7F) + offset);
-	organlist->SetItem(i, 3, temp);
-	organlist->SetItemData(i, j);
-
-}
-
 void SettingsDialog::OnChanged(wxCommandEvent& event)
 {
 
@@ -661,35 +295,6 @@ void SettingsDialog::OnDevicesMIDIDoubleClick(wxCommandEvent& event)
 	}
 }
 
-void SettingsDialog::OnEventListClick(wxListEvent& event)
-{
-	page2button->Enable();
-}
-
-void SettingsDialog::OnEventListDoubleClick(wxListEvent& event)
-{
-
-	page2button->Enable();
-
-	int index = page2list->GetFirstSelected();
-
-	MIDIListenDialog dlg(this, page2list->GetItemText(index), GetEventType(index), page2list->GetItemData(index));
-
-	if (dlg.ShowModal() == wxID_OK)
-	{
-		SetEventData(index, dlg.GetEvent());
-		UpdateMessages(index);
-		OnChanged(event);
-	}
-
-}
-
-void SettingsDialog::OnEventListDoubleClickC(wxCommandEvent& event)
-{
-    wxListEvent listevent;
-	OnEventListDoubleClick(listevent);
-}
-
 void SettingsDialog::OnApply(wxCommandEvent& event)
 {
 	DoApply();
@@ -704,12 +309,6 @@ void SettingsDialog::OnHelp(wxCommandEvent& event)
 
 void SettingsDialog::OnOK(wxCommandEvent& event)
 {
-	std::map<long, wxString> list;
-	for (int i=0; i<organlist->GetItemCount(); i++)
-		list.insert(std::pair<long, wxString>(organlist->GetItemData(i), organlist->GetItemText(i)));
-
-	m_Settings.SetOrganList(list);
-
 	if (DoApply())
 		event.Skip();
 
@@ -720,6 +319,9 @@ bool SettingsDialog::DoApply()
 
 	if (!(this->Validate()))
 		return false;
+
+	m_OptionsPage->Save();
+	m_OrganPage->Save();
 
 	for (size_t i = 0; i < page1checklist->GetCount(); i++)
 	{
@@ -733,22 +335,12 @@ bool SettingsDialog::DoApply()
 	m_Settings.SetDefaultAudioDevice(c_sound->GetStringSelection());
 	m_Settings.SetAudioDeviceLatency(c_sound->GetStringSelection(), c_latency->GetValue());
 	m_Settings.SetLoadInStereo(c_stereo->GetSelection());
-	m_Settings.SetLosslessCompression(c_squash->IsChecked());
-	m_Settings.SetManagePolyphony(c_limit->IsChecked());
-	m_Settings.SetCompressCache(c_CompressCache->IsChecked());
-	m_Settings.SetScaleRelease(c_scale->IsChecked());
-	m_Settings.SetRandomizeSpeaking(c_random->IsChecked());
 	m_Settings.SetSampleRate(wxAtoi(c_SampleRate->GetStringSelection()));
 	m_Settings.SetBitsPerSample(c_BitsPerSample->GetSelection() * 4 + 8);
-	m_Settings.SetConcurrency(c_Concurrency->GetSelection());
-	m_Settings.SetReleaseConcurrency(c_ReleaseConcurrency->GetSelection() + 1);
-	m_Settings.SetWaveFormatBytesPerSample(c_WaveFormat->GetSelection() + 1);
 	m_Settings.SetLoopLoad(c_LoopLoad->GetSelection());
 	m_Settings.SetAttackLoad(c_AttackLoad->GetSelection());
 	m_Settings.SetReleaseLoad(c_ReleaseLoad->GetSelection());
 	m_Settings.SetInterpolationType(c_Interpolation->GetSelection());
-	m_Settings.SetUserSettingPath(c_SettingsPath->GetPath());
-	m_Settings.SetUserCachePath(c_CachePath->GetPath());
 
 	m_Sound.ResetSound();
 	UpdateSoundStatus();
--- src/grandorgue/SettingsDialog.h
+++ src/grandorgue/SettingsDialog.h
@@ -24,25 +24,15 @@
 #define SETTINGSDIALOG_H
 
 #include <vector>
+#include <wx/wx.h>
 #include <wx/propdlg.h>
-
-class wxButton;
-class wxCheckBox;
-class wxCheckListBox;
-class wxChoice;
-class wxDirPickerCtrl;
-class wxListEvent;
-class wxListView;
-class wxPanel;
-class wxSpinCtrl;
-class wxSpinEvent;
-class wxStaticText;
-class wxToggleButton;
 
 class GOrgueSound;
 class GOrgueSettings;
 
-#include "MIDIListenDialog.h"
+class SettingsMidiMessage;
+class SettingsOption;
+class SettingsOrgan;
 
 class SettingsDialog : public wxPropertySheetDialog
 {
@@ -52,23 +42,16 @@ private:
 	GOrgueSettings& m_Settings;
 
 	void SetLatencySpinner(int latency);
-	MIDIListenDialog::LISTEN_DIALOG_TYPE GetEventType(unsigned index);
-	unsigned GetEventCount();
-	wxString GetEventName(unsigned index);
-	int GetEventData(unsigned index);
-	void SetEventData(unsigned index, int event);
+
+	wxPanel* CreateDevicesPage(wxWindow* parent);
+
+	void UpdateSoundStatus();
+
+	bool DoApply();
 
 public:
 	SettingsDialog(wxWindow* parent, GOrgueSound& sound);
 	~SettingsDialog();
-
-	void UpdateSoundStatus();
-	wxPanel* CreateDevicesPage(wxWindow* parent);
-	wxPanel* CreateOptionsPage(wxWindow* parent);
-	wxPanel* CreateMessagesPage(wxWindow* parent);
-	wxPanel* CreateOrganPage(wxWindow* parent);
-	void UpdateMessages(unsigned i);
-	void UpdateOrganMessages(int i);
 
 	void OnChanged(wxCommandEvent& event);
 	void OnDevicesSoundChoice(wxCommandEvent& event);
@@ -76,18 +59,7 @@ public:
 	void OnDevicesMIDIDoubleClick(wxCommandEvent& event);
 	virtual void OnApply(wxCommandEvent& event);
 	virtual void OnOK(wxCommandEvent& event);
-    void OnHelp(wxCommandEvent& event);
-	bool DoApply();
-
-	void OnEventListClick(wxListEvent& event);
-	void OnEventListDoubleClick(wxListEvent& event);
-	void OnEventListDoubleClickC(wxCommandEvent& event);
-
-	void OnOrganEventListClick(wxListEvent& event);
-	void OnOrganEventListDoubleClick(wxListEvent& event);
-	void OnAddOrgan(wxCommandEvent& event);
-	void OnDelOrgan(wxCommandEvent& event);
-	void OnOrganProperties(wxCommandEvent& event);
+	void OnHelp(wxCommandEvent& event);
 
 	void OnLatencySpinnerChange(wxSpinEvent& event);
 
@@ -101,28 +73,12 @@ protected:
 		ID_LATENCY,
 		ID_POLYPHONY,
 		ID_MONO_STEREO,
+		ID_BITS_PER_SAMPLE,
 		ID_SAMPLE_RATE,
-		ID_CONCURRENCY,
-		ID_RELEASE_CONCURRENCY,
-		ID_WAVE_FORMAT,
 		ID_INTERPOLATION,
 		ID_LOOP_LOAD,
 		ID_ATTACK_LOAD,
 		ID_RELEASE_LOAD,
-		ID_ENHANCE_SQUASH,
-		ID_ENHANCE_MANAGE_POLYPHONY,
-		ID_COMPRESS_CACHE,
-		ID_ENHANCE_SCALE_RELEASE,
-		ID_ENHANCE_RANDOMIZE,
-		ID_SETTINGS_DIR,
-		ID_CACHE_DIR,
-
-		ID_MIDI_EVENTS,
-		ID_EVENT_PROPERTIES,
-		ID_ORGAN_EVENTS,
-		ID_EVENT_DEL,
-		ID_EVENT_ADD,
-		ID_EVENT_ORGANPROPERTIES
 	};
 
 	wxCheckListBox* page1checklist;
@@ -137,24 +93,19 @@ protected:
 	wxChoice* c_stereo;
 	wxChoice* c_SampleRate;
 	wxChoice* c_BitsPerSample;
-	wxChoice* c_Concurrency;
-	wxChoice* c_ReleaseConcurrency;
-	wxChoice* c_WaveFormat;
 	wxChoice* c_Interpolation;
 	wxChoice* c_LoopLoad;
 	wxChoice* c_AttackLoad;
 	wxChoice* c_ReleaseLoad;
-	wxCheckBox *c_squash, *c_limit, *c_CompressCache, *c_scale, *c_random;
-	wxDirPickerCtrl* c_SettingsPath;
-	wxDirPickerCtrl* c_CachePath;
-	bool b_squash, b_stereo;
+	bool b_stereo;
 	unsigned m_OldBitsPerSample;
 	unsigned m_OldLoopLoad;
 	unsigned m_OldAttackLoad;
 	unsigned m_OldReleaseLoad;
 
-	wxListView* page2list, *organlist;
-	wxButton* page2button, *addButton, *delButton, *propButton;
+	SettingsOption* m_OptionsPage;
+	SettingsOrgan* m_OrganPage;
+	SettingsMidiMessage* m_MidiMessagePage;
 };
 
 
