924d31522a3a6b2ae92f0a6dc04fff0fcff0a99d
Martin Koegler <martin.koegler@chello.at>
2012-07-22 08:47:46 +0000

    Fill combination definitions
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1054 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GOrgueCombinationDefinition.cpp
+++ src/grandorgue/GOrgueCombinationDefinition.cpp
@@ -20,6 +20,12 @@
  */
 
 #include "GOrgueCombinationDefinition.h"
+#include "GOrgueCoupler.h"
+#include "GOrgueDivisionalCoupler.h"
+#include "GOrgueManual.h"
+#include "GOrgueStop.h"
+#include "GOrgueTremulant.h"
+#include "GrandOrgueFile.h"
 
 GOrgueCombinationDefinition::GOrgueCombinationDefinition(GrandOrgueFile* organfile) :
 	m_organfile(organfile),
@@ -34,11 +40,100 @@ GOrgueCombinationDefinition::~GOrgueCombinationDefinition()
 void GOrgueCombinationDefinition::InitGeneral()
 {
 	m_Content.resize(0);
+
+	for (unsigned j = m_organfile->GetFirstManualIndex(); j <= m_organfile->GetManualAndPedalCount(); j++)
+	{
+		for (unsigned i = 0; i < m_organfile->GetManual(j)->GetStopCount(); i++)
+		{
+			CombinationSlot def;
+			def.type = COMBINATION_STOP;
+			def.manual = j;
+			def.index = i + 1;
+			def.store_unconditional = m_organfile->CombinationsStoreNonDisplayedDrawstops() || m_organfile->GetManual(j)->GetStop(i)->IsDisplayed();
+			def.control = m_organfile->GetManual(j)->GetStop(i);
+			def.group = m_organfile->GetManual(j)->GetName();
+			m_Content.push_back(def);
+		}
+	}
+	
+	for (unsigned j = m_organfile->GetFirstManualIndex(); j <= m_organfile->GetManualAndPedalCount(); j++)
+	{
+		for (unsigned i = 0; i < m_organfile->GetManual(j)->GetCouplerCount(); i++)
+		{
+			CombinationSlot def;
+			def.type = COMBINATION_COUPLER;
+			def.manual = j;
+			def.index = i + 1;
+			def.store_unconditional = m_organfile->CombinationsStoreNonDisplayedDrawstops() || m_organfile->GetManual(j)->GetCoupler(i)->IsDisplayed();
+			def.control = m_organfile->GetManual(j)->GetCoupler(i);
+			def.group = m_organfile->GetManual(j)->GetName();
+			m_Content.push_back(def);
+		}
+	}
+
+	for (unsigned i = 0; i < m_organfile->GetTremulantCount(); i++)
+	{
+		CombinationSlot def;
+		def.type = COMBINATION_TREMULANT;
+		def.manual = -1;
+		def.index = i + 1;
+		def.store_unconditional = m_organfile->CombinationsStoreNonDisplayedDrawstops() || m_organfile->GetTremulant(i)->IsDisplayed();
+		def.control = m_organfile->GetTremulant(i);
+		m_Content.push_back(def);
+	}
+
+	for (unsigned i = 0; i < m_organfile->GetDivisionalCouplerCount(); i++)
+	{
+		CombinationSlot def;
+		def.type = COMBINATION_DIVISIONALCOUPLER;
+		def.manual = -1;
+		def.index = i + 1;
+		def.store_unconditional = m_organfile->GeneralsStoreDivisionalCouplers();
+		def.control = m_organfile->GetDivisionalCoupler(i);
+		m_Content.push_back(def);
+	}
 }
 
 void GOrgueCombinationDefinition::InitDivisional(unsigned manual_number)
 {
+	GOrgueManual* associatedManual = m_organfile->GetManual(manual_number);
 	m_Content.resize(0);
+
+	for (unsigned i = 0; i < associatedManual->GetStopCount(); i++)
+	{
+		CombinationSlot def;
+		def.type = COMBINATION_STOP;
+		def.manual = manual_number;
+		def.index = i + 1;
+		def.store_unconditional = m_organfile->CombinationsStoreNonDisplayedDrawstops() || associatedManual->GetStop(i)->IsDisplayed();
+		def.group = associatedManual->GetName();
+		def.control = associatedManual->GetStop(i);
+		m_Content.push_back(def);
+	}
+	for (unsigned i = 0; i < associatedManual->GetCouplerCount(); i++)
+	{
+		CombinationSlot def;
+		def.type = COMBINATION_COUPLER;
+		def.manual = manual_number;
+		def.index = i + 1;
+		def.store_unconditional = ((m_organfile->DivisionalsStoreIntramanualCouplers() && !associatedManual->GetCoupler(i)->IsIntermanual()) || 
+					   (m_organfile->DivisionalsStoreIntermanualCouplers() && associatedManual->GetCoupler(i)->IsIntermanual())) && 
+			(m_organfile->CombinationsStoreNonDisplayedDrawstops() || associatedManual->GetCoupler(i)->IsDisplayed());
+		def.group = associatedManual->GetName();
+		def.control = associatedManual->GetCoupler(i);
+		m_Content.push_back(def);
+	}
+	for (unsigned i = 0; i < associatedManual->GetTremulantCount(); i++)
+	{
+		CombinationSlot def;
+		def.type = COMBINATION_TREMULANT;
+		def.manual = manual_number;
+		def.index = i + 1;
+		def.store_unconditional = m_organfile->DivisionalsStoreTremulants() && (m_organfile->CombinationsStoreNonDisplayedDrawstops() || associatedManual->GetTremulant(i)->IsDisplayed());
+		def.group = associatedManual->GetName();
+		def.control = associatedManual->GetTremulant(i);
+		m_Content.push_back(def);
+	}
 }
 
 const std::vector<GOrgueCombinationDefinition::CombinationSlot>& GOrgueCombinationDefinition::GetCombinationElements()
