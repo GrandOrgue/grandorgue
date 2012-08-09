9d20827c78dd45091d25eeb41aa9e9e258c0ce15
Martin Koegler <martin.koegler@chello.at>
2012-08-09 16:40:47 +0000

    Use unique output names
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1062 69509848-2a73-49d5-a893-c4d8c934025a

--- src/rt/rtmidi/RtMidi.cpp
+++ src/rt/rtmidi/RtMidi.cpp
@@ -1426,6 +1426,8 @@ std::string RtMidiOut :: getPortName( unsigned int portNumber )
     snd_seq_get_any_client_info( data->seq, cnum, cinfo );
     std::ostringstream os;
     os << snd_seq_client_info_get_name(cinfo);
+    os << " ";                                    // GO: These lines added to make sure devices are listed
+    os << snd_seq_port_info_get_client( pinfo );  // GO: with full portnames added to ensure individual device names
     os << ":";
     os << snd_seq_port_info_get_port(pinfo);
     stringName = os.str();
@@ -2366,6 +2368,14 @@ std::string RtMidiOut :: getPortName( unsigned int portNumber )
 #else
   stringName = std::string( deviceCaps.szPname );
 #endif
+
+  // GO: Next lines added to add the portNumber to the name so that 
+  // GO: the device's names are sure to be listed with individual names
+  // GO: even when they have the same brand name
+  std::ostringstream os;
+  os << " ";
+  os << portNumber;
+  stringName += os.str();
 
   return stringName;
 }
