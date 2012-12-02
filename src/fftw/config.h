310ce94c6078194da60a0ddcc0357d25dd4da05d
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2012-12-02 16:30:13 +0000

    Include fftw in the build system
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1154 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/fftw/config.h
@@ -0,0 +1,9 @@
+#define HAVE_UINTPTR_T
+#define FFTW_SINGLE
+#define WITH_OUR_MALLOC
+#define PACKAGE "fftw"
+#define VERSION "0"
+#define PACKAGE_VERSION "0"
+#define FFTW_CC "gcc"
+
+#define HAVE_STRING_H
