a1dca2b893ab1f9ffe04b1711acc4ebe4328b61b
Martin Koegler <martin.koegler@chello.at>
2012-12-29 17:22:52 +0000

    Add GOrgueConfigFileReader
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1188 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueConfigFileReader.cpp
@@ -0,0 +1,129 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2012 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "GOrgueConfigFileReader.h"
+#include <wx/file.h>
+
+GOrgueConfigFileReader::GOrgueConfigFileReader() :
+	m_Entries()
+{
+}
+
+GOrgueConfigFileReader::~GOrgueConfigFileReader()
+{
+}
+
+const std::map<wxString, std::map<wxString, wxString> >& GOrgueConfigFileReader::GetContent()
+{
+	return m_Entries;
+}
+
+wxString GOrgueConfigFileReader::GetNextLine(const wxString& buffer, unsigned &pos)
+{
+	unsigned newpos = buffer.find(wxT("\n"), pos);
+	wxString line = buffer.Mid(pos, newpos - pos);
+	pos = newpos + 1;
+	if (line.Len() > 0 && line[line.Len() - 1] == wxT('\r'))
+		return line.Mid(0, line.Len() - 1);
+	return line;
+}
+
+wxString GOrgueConfigFileReader::getEntry(wxString group, wxString name)
+{
+	std::map<wxString, std::map<wxString, wxString> >::const_iterator i = m_Entries.find(group);
+	if (i == m_Entries.end())
+		return wxEmptyString;
+	const std::map<wxString, wxString>& g = i->second;
+	std::map<wxString, wxString>::const_iterator j = g.find(name);
+	if (j == g.end())
+		return wxEmptyString;
+	else
+		return j->second;
+}
+
+bool GOrgueConfigFileReader::Read(wxString filename)
+{
+	wxFile file;
+	m_Entries.clear();
+	
+	if (!file.Open(filename, wxFile::read))
+	{
+		wxLogError(_("Failed to open file '%s'"), filename.c_str());
+		return false;
+	}
+	wxFileOffset length = file.Length();
+	char* data = new char[length];
+	if (file.Read(data, length) != length)
+	{
+		delete[] data;
+		wxLogError(_("Failed to read file '%s'"), filename.c_str());
+		return false;
+	}
+	wxString input(data, wxCSConv(wxT("ISO-8859-1")), length);
+	delete[] data;
+	
+	unsigned pos = 0;
+	wxString group;
+	std::map<wxString, wxString>* grp = NULL;
+	unsigned lineno = 1;
+	
+	while(pos < input.Len())
+	{
+		wxString line = GetNextLine(input, pos);
+		lineno++;
+		
+		if (line == wxEmptyString)
+			continue;
+		if (line.Len() > 1 && line[0] == wxT(';'))
+			continue;
+		if (line.Len() > 1 && line[0] == wxT('[') && line[line.Len() - 1] == wxT(']'))
+		{
+			group = line.Mid(1, line.Len() - 2);
+			if (m_Entries.find(group) != m_Entries.end())
+			{
+				wxLogWarning(_("Dupplicate group at line %d: %s"), lineno, group.c_str());
+			}
+			grp = &m_Entries[group];
+		}
+		else
+		{
+			if (!grp)
+			{
+				wxLogError(_("Config entry without any group at line %d"), lineno);
+				continue;
+			}
+			unsigned datapos = line.find(wxT("="), 0);
+			if (datapos <= 0)
+			{
+				wxLogError(_("Invalid Config entry at line %d: %s"), lineno, line.c_str());
+				continue;
+			}
+			wxString name = line.Mid(0, datapos);
+			if (grp->find(name) != grp->end())
+			{
+				wxLogWarning(_("Dupplicate entry at line %d: %s"), lineno, name.c_str());
+			}
+			(*grp)[name] = line.Mid(datapos + 1);
+		}
+	}
+
+	return true;
+}
