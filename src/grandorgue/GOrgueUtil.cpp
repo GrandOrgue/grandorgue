19ec12ea10ec42a34da9f4820144b6efcaaf9da2
Martin Koegler <martin.koegler@chello.at>
2013-02-11 08:10:21 +0000

    Locale independent float parser
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1240 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GOrgueUtil.cpp
+++ src/grandorgue/GOrgueUtil.cpp
@@ -21,14 +21,73 @@
 
 #include "GOrgueUtil.h"
 
+static wxChar digits[] = { wxT('0'), wxT('1'), wxT('2'), wxT('3'), wxT('4'), wxT('5'), wxT('6'), wxT('7'), wxT('8'), wxT('9') };
+
 wxString formatCDDouble(double value)
 {
-	return wxString::Format(wxT("%f"), value);
+	wxString result = wxEmptyString;
+	unsigned vk = 1;
+	if (value < 0)
+	{
+		result = wxT('-');
+		value = -value;
+	}
+	value += 0.00000005;
+	while (value >= 10.0)
+	{
+		vk++;
+		value = value / 10;
+	}
+	for(unsigned i = 0; i < vk; i++)
+	{		
+		unsigned val = (unsigned) value;
+		result += digits[val];
+		value = (value - val) * 10;
+	}		
+	result += wxT(".");
+	for(unsigned i = 0; i < 8 - vk; i++)
+	{		
+		unsigned val = (unsigned) value;
+		result += digits[val];
+		value = (value - val) * 10;
+	}		
+	return result;
 }
 
 bool parseCDouble(double& result, wxString value)
 {
-	if (!value.ToDouble(&result))
+	bool sign = false;
+	unsigned pos;
+	double shift = 0.1;
+	result = 0;
+	if (value.length() > 0 && value[0] == wxT('-'))
+	{
+		sign = true;
+		value = value.Mid(1);
+	}
+	if (value.length() < 1)
 		return false;
+	for (pos = 0; pos < value.length(); pos++)
+	{
+		if (value[pos] == wxT('.'))
+		{
+			if (pos == 0)
+				return false;
+			pos++;
+			break;
+		}
+		if (value[pos] < wxT('0') || wxT('9') < value[pos])
+			return false;
+		result = result * 10 + (value[pos] - wxT('0'));
+	}
+	for (; pos < value.length(); pos++)
+	{
+		if (value[pos] < wxT('0') || wxT('9') < value[pos])
+			return false;
+		result = result  + shift * (value[pos] - wxT('0'));
+		shift = shift / 10;
+	}
+	if (sign)
+		result = -result;
 	return true;
 }
