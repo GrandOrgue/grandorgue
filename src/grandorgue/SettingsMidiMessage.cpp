a3ab3c89d6b44cf70bc7eb9aa8280f2f447734c5
Martin Koegler <martin.koegler@chello.at>
2013-11-10 15:53:50 +0000

    Remove old initial MIDI configuration tab (part 1)
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1391 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/SettingsMidiMessage.cpp
+++ src/grandorgue/SettingsMidiMessage.cpp
@@ -48,12 +48,6 @@ SettingsMidiMessage::SettingsMidiMessage(GOrgueSettings& settings, GOrgueMidi& m
 	m_Properties->Disable();
 	topSizer->Add(m_Properties, 0, wxALIGN_RIGHT | wxALL, 5);
 
-	for (unsigned i = 0; i < GetEventCount(); i++)
-	{
-		m_Events->InsertItem(i, GetEventName(i));
-		UpdateMessages(i);
-	}
-
 	topSizer->AddSpacer(5);
 	this->SetSizer(topSizer);
 	topSizer->Fit(this);
@@ -62,111 +56,6 @@ SettingsMidiMessage::SettingsMidiMessage(GOrgueSettings& settings, GOrgueMidi& m
 	m_Events->SetColumnWidth(1, 300);
 	m_Events->SetColumnWidth(2, 62);
 	m_Events->SetColumnWidth(3, 100);
-}
-
-unsigned SettingsMidiMessage::GetEventCount()
-{
-	return m_Settings.GetSetterCount() + m_Settings.GetEnclosureCount() +
-		m_Settings.GetManualCount() + 1;
-}
-
-MIDIListenDialog::LISTEN_DIALOG_TYPE SettingsMidiMessage::GetEventType(unsigned index)
-{
-	if (index < m_Settings.GetSetterCount())
-		return MIDIListenDialog::LSTN_SETTINGSDLG_MEMORY_OR_ORGAN;
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-		return MIDIListenDialog::LSTN_ENCLOSURE;
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-		return MIDIListenDialog::LSTN_MANUAL;
-	index -= m_Settings.GetManualCount();
-
-	return MIDIListenDialog::LSTN_SETTINGSDLG_STOP_CHANGE;
-}
-
-wxString SettingsMidiMessage::GetEventName(unsigned index)
-{
-	if (index < m_Settings.GetSetterCount())
-		return m_Settings.GetSetterTitle(index);
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-		return m_Settings.GetEnclosureTitle(index);
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-		return m_Settings.GetManualTitle(index);
-	index -= m_Settings.GetManualCount();
-
-	return m_Settings.GetStopChangeTitle();
-}
-
-int SettingsMidiMessage::GetEventData(unsigned index)
-{
-	if (index < m_Settings.GetSetterCount())
-		return m_Settings.GetSetterEvent(index);
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-		return m_Settings.GetEnclosureEvent(index);
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-		return m_Settings.GetManualEvent(index);
-	index -= m_Settings.GetManualCount();
-
-	return m_Settings.GetStopChangeEvent();
-}
-
-void SettingsMidiMessage::SetEventData(unsigned index, int event)
-{
-	if (index < m_Settings.GetSetterCount())
-	{
-		m_Settings.SetSetterEvent(index, event);
-		return;
-	}
-	index -= m_Settings.GetSetterCount();
-
-	if (index < m_Settings.GetEnclosureCount())
-	{
-		m_Settings.SetEnclosureEvent(index, event);
-		return;
-	}
-	index -= m_Settings.GetEnclosureCount();
-
-	if (index < m_Settings.GetManualCount())
-	{
-		m_Settings.SetManualEvent(index, event);
-		return;
-	}
-	index -= m_Settings.GetManualCount();
-
-	m_Settings.SetStopChangeEvent(event);
-}
-
-void SettingsMidiMessage::UpdateMessages(unsigned i)
-{
-	MIDIListenDialog::LISTEN_DIALOG_TYPE type = GetEventType(i);
-	int j = GetEventData(i);
-	m_Events->SetItem(i, 1, MIDIListenDialog::GetEventTitle(j, type));
-	m_Events->SetItem(i, 2, MIDIListenDialog::GetEventChannelString(j));
-
-	if (type != MIDIListenDialog::LSTN_SETTINGSDLG_STOP_CHANGE && j)
-	{
-		int offset = (j & 0xF000) == 0xC000 ? 1 : 0;
-		wxString temp;
-		if (type == 1 ) // when type == 1 (keyboards) data contained in lower byte correspond to note offset
-		{
-			if ((j&0xFF)>0x7F) offset = -140; // negative numbers are coded as 128=-12 140=0
-		}
-		temp << ((j & 0xFF) + offset );
-		m_Events->SetItem(i, 3, temp);
-	}
-
-	m_Events->SetItemData(i, j);
 }
 
 void SettingsMidiMessage::OnEventsClick(wxListEvent& event)
@@ -179,13 +68,6 @@ void SettingsMidiMessage::OnEventsDoubleClick(wxListEvent& event)
 	m_Properties->Enable();
 	int index = m_Events->GetFirstSelected();
 
-	MIDIListenDialog dlg(this, m_Events->GetItemText(index), m_midi, GetEventType(index), m_Events->GetItemData(index));
-
-	if (dlg.ShowModal() == wxID_OK)
-	{
-		SetEventData(index, dlg.GetEvent());
-		UpdateMessages(index);
-	}
 }
 
 void SettingsMidiMessage::OnProperties(wxCommandEvent& event)
