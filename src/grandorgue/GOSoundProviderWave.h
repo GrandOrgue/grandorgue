89f4eab9037d80aaa910770753f5d94d79b09553
Martin Koegler <martin.koegler@chello.at>
2013-12-19 09:52:33 +0000

    Use const for strings
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1487 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GOSoundProviderWave.h
+++ src/grandorgue/GOSoundProviderWave.h
@@ -82,13 +82,13 @@ class GOSoundProviderWave : public GOSoundProvider
 	void CreateAttack(const char* data, GOrgueWave& wave, int attack_start, std::vector<GO_WAVE_LOOP> loop_list, int sample_group, unsigned bits_per_sample, 
 			  unsigned channels, bool compress, loop_load_type loop_mode, bool percussive, unsigned min_attack_velocity);
 	void CreateRelease(const char* data, GOrgueWave& wave, int sample_group, unsigned max_playback_time, int cue_point, int release_end, unsigned bits_per_sample, unsigned channels, bool compress);
-	void ProcessFile(wxString filename, wxString path, std::vector<GO_WAVE_LOOP> loops, bool is_attack, bool is_release, int sample_group, unsigned max_playback_time, int attack_start, int cue_point, 
+	void ProcessFile(const wxString& filename, const wxString& path, std::vector<GO_WAVE_LOOP> loops, bool is_attack, bool is_release, int sample_group, unsigned max_playback_time, int attack_start, int cue_point, 
 			 int release_end, unsigned bits_per_sample, int load_channels, bool compress, loop_load_type loop_mode, bool percussive, unsigned min_attack_velocity);
 
 public:
 	GOSoundProviderWave(GOrgueMemoryPool& pool);
 
-	void LoadFromFile(std::vector<attack_load_info> attacks, std::vector<release_load_info> releases, wxString path, unsigned bits_per_sample, int channels, bool compress, loop_load_type loop_mode,
+	void LoadFromFile(std::vector<attack_load_info> attacks, std::vector<release_load_info> releases, const wxString& path, unsigned bits_per_sample, int channels, bool compress, loop_load_type loop_mode,
 			  unsigned attack_load, unsigned release_load, int midi_key_number);
 	void SetAmplitude(float fixed_amplitude, float gain);
 };
