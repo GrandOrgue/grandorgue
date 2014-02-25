3ab719afdedd8523684ae71b20fc24f689dafe8c
Martin Koegler <martin.koegler@chello.at>
2014-02-25 21:51:31 +0000

    Add missing file
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1613 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueMidiFile.h
@@ -0,0 +1,43 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2014 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GORGUEMIDIFILE_H
+#define GORGUEMIDIFILE_H
+
+#include <stdint.h>
+
+#pragma pack(push, 1)
+
+typedef struct {
+	char type[4];
+	uint32_t len;
+} MIDIFileHeader;
+
+typedef struct {
+	MIDIFileHeader header;
+	uint16_t type;
+	uint16_t tracks;
+	uint16_t ppq;
+} MIDIHeaderChunk;
+
+#pragma pack(pop)
+
+#endif
