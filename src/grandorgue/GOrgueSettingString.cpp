3439b66dc9a40874263ae0110061e46ba2fd37ec
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2015-04-09 20:32:37 +0000

    Add string setting
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1892 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueSettingString.cpp
@@ -0,0 +1,67 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "GOrgueSettingString.h"
+
+#include "GOrgueConfigReader.h"
+#include "GOrgueConfigWriter.h"
+
+GOrgueSettingString::GOrgueSettingString(GOrgueSettingStore* store, wxString group, wxString name, wxString default_value) :
+	GOrgueSetting(store, group, name),
+	m_Value(default_value),
+	m_DefaultValue(default_value)
+{
+}
+
+void GOrgueSettingString::Load(GOrgueConfigReader& cfg)
+{
+	(*this)(cfg.ReadString(CMBSetting, m_Group, m_Name, false, m_DefaultValue));
+}
+
+void GOrgueSettingString::Save(GOrgueConfigWriter& cfg)
+{
+	cfg.WriteString(m_Group, m_Name, m_Value);
+}
+
+void GOrgueSettingString::setDefaultValue(wxString default_value)
+{
+	m_DefaultValue = default_value;
+}
+
+wxString GOrgueSettingString::getDefaultValue()
+{
+	return m_DefaultValue;
+}
+
+wxString GOrgueSettingString::operator() () const
+{
+	return m_Value;
+}
+
+void GOrgueSettingString::operator()(wxString value)
+{
+	m_Value = validate(value);
+}
+
+wxString GOrgueSettingString::validate(wxString value)
+{
+	return value;
+}
