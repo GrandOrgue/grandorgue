c4b99aec0296afc30a928ef37aa39686fd8a2a7e
Martin Koegler <martin.koegler@chello.at>
2015-07-27 07:06:20 +0000

    Add file wrapper classes
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1931 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueInvalidFile.cpp
@@ -0,0 +1,60 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "GOrgueInvalidFile.h"
+
+GOrgueInvalidFile::GOrgueInvalidFile(const wxString& name) :
+	m_Name(name)
+{
+}
+
+const wxString GOrgueInvalidFile::GetName()
+{
+	return m_Name;
+}
+
+const wxString GOrgueInvalidFile::GetPath()
+{
+	return wxEmptyString;
+}
+
+bool GOrgueInvalidFile::isValid()
+{
+	return false;
+}
+
+size_t GOrgueInvalidFile::GetSize()
+{
+	return 0;
+}
+
+bool GOrgueInvalidFile::Open()
+{
+	return false;
+}
+void GOrgueInvalidFile::Close()
+{
+}
+
+size_t GOrgueInvalidFile::Read(void * buffer, size_t len)
+{
+	return 0;
+}
---
+++ src/grandorgue/GOrgueStandardFile.cpp
@@ -0,0 +1,76 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "GOrgueStandardFile.h"
+
+GOrgueStandardFile::GOrgueStandardFile(const wxString& path) :
+	m_Path(path),
+	m_Name(path),
+	m_Size(0)
+{
+}
+
+GOrgueStandardFile::GOrgueStandardFile(const wxString& path, const wxString& name) :
+	m_Path(path),
+	m_Name(name),
+	m_Size(0)
+{
+}
+
+GOrgueStandardFile::~GOrgueStandardFile()
+{
+	Close();
+}
+
+const wxString GOrgueStandardFile::GetName()
+{
+	return m_Name;
+}
+
+const wxString GOrgueStandardFile::GetPath()
+{
+	return m_Path;
+}
+
+size_t GOrgueStandardFile::GetSize()
+{
+	return m_Size;
+}
+
+bool GOrgueStandardFile::Open()
+{
+	if (!m_File.Open(m_Path, wxFile::read))
+		return false;
+	m_Size = m_File.Length();
+	return true;
+}
+void GOrgueStandardFile::Close()
+{
+	m_File.Close();
+}
+
+size_t GOrgueStandardFile::Read(void * buffer, size_t len)
+{
+	ssize_t read = m_File.Read(buffer, len);
+	if (read == wxInvalidOffset)
+		return 0;
+	return read;
+}
