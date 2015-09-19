c07c15f23ec4de7fe5b376b2fdec062d9ea6297d
Martin Koegler <martin.koegler@chello.at>
2015-09-19 08:57:33 +0000

    Refactor cmake help macros
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1957 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ cmake/BuildHelp.cmake
@@ -0,0 +1,52 @@
+# GrandOrgue - free pipe organ simulator
+# 
+# Copyright 2006 Milan Digital Audio LLC
+# Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+function(BUILD_HELPFILE xmlfile VARIANT)
+  SET(HELPDIR "${RESOURCEDIR}/help")
+  SET(HELPINSTDIR "${RESOURCEINSTDIR}/help")
+
+  SET(BUILDDIR ${CMAKE_CURRENT_BINARY_DIR}/help${VARIANT})
+  SET(SRCDIR ${CMAKE_CURRENT_SOURCE_DIR})
+  SET(IMGDIR ${SRCDIR}/images)
+  FILE(GLOB_RECURSE IMAGES RELATIVE ${SRCDIR} ${IMGDIR}/*.gif ${IMGDIR}/*.jpg ${IMGDIR}/*.png)
+  file(MAKE_DIRECTORY ${BUILDDIR})
+  file(MAKE_DIRECTORY ${HELPDIR})
+
+  SET(_ImgList)
+  FOREACH(IMG_NAME ${IMAGES})
+    ADD_CUSTOM_COMMAND(OUTPUT ${BUILDDIR}/${IMG_NAME} COMMAND ${CMAKE_COMMAND} -E copy ${SRCDIR}/${IMG_NAME} ${BUILDDIR}/${IMG_NAME} DEPENDS ${SRCDIR}/${IMG_NAME})
+    SET(_ImgList ${_ImgList} ${BUILDDIR}/${IMG_NAME})
+  ENDFOREACH()
+  
+  ADD_CUSTOM_COMMAND(OUTPUT ${BUILDDIR}/GrandOrgue.hhp COMMAND ${XSLTPROC}
+    ARGS --path ${DOCBOOK_PATH}/htmlhelp ${SRCDIR}/grandorgue.xsl ${xmlfile}
+    WORKING_DIRECTORY ${BUILDDIR} DEPENDS ${SRCDIR}/grandorgue.xsl ${xmlfile})
+
+  ADD_CUSTOM_COMMAND(OUTPUT ${HELPDIR}/GrandOrgue${VARIANT}.htb COMMAND ${ZIP} 
+    ARGS -r --filesync ${HELPDIR}/GrandOrgue${VARIANT}.htb * WORKING_DIRECTORY ${BUILDDIR}
+    DEPENDS ${_ImgList} ${BUILDDIR}/GrandOrgue.hhp)
+
+  INSTALL(FILES ${HELPDIR}/GrandOrgue${VARIANT}.htb DESTINATION ${HELPINSTDIR})
+  ADD_CUSTOM_TARGET(build-helpfile${VARIANT} DEPENDS ${HELPDIR}/GrandOrgue${VARIANT}.htb)
+
+  IF (NOT TARGET build-help)
+      ADD_CUSTOM_TARGET(build-help ALL)
+  ENDIF ()
+  ADD_DEPENDENCIES(build-help build-helpfile${VARIANT})
+endfunction()
---
+++ cmake/TranslateHelp.cmake
@@ -0,0 +1,33 @@
+# GrandOrgue - free pipe organ simulator
+# 
+# Copyright 2006 Milan Digital Audio LLC
+# Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+function(PO4A_BUILD_HELP xmlfile)
+  FOREACH(_currentPoFile ${ARGN})
+    GET_FILENAME_COMPONENT(_lang ${_currentPoFile} NAME_WE)
+    ADD_CUSTOM_COMMAND(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_lang}.xml COMMAND ${PO4A_TRANSLATE}
+      ARGS -f docbook -m ${xmlfile} -p ${_currentPoFile} -l ${CMAKE_CURRENT_BINARY_DIR}/${_lang}.xml -k 0
+      DEPENDS ${_currentPoFile} ${xmlfile})
+          BUILD_HELPFILE(${CMAKE_CURRENT_BINARY_DIR}/${_lang}.xml _${_lang})
+  ENDFOREACH()
+endfunction()
+
+function(PO4A_UPDATE_POT target _potFile xmlfile)
+  ADD_CUSTOM_TARGET(${target} COMMAND ${PO4A_GETTEXT} ARGS -f docbook -m ${xmlfile} -p ${CMAKE_CURRENT_BINARY_DIR}/${_potFile} --package-name=GrandOrgue --msgid-bugs-address="ourorgan-developers@lists.sourceforge.net" WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+endfunction()
+
