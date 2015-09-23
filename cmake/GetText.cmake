e469b9b96e554110f027485210fffe9c596ae27a
Martin Koegler <martin.koegler@chello.at>
2015-09-23 06:50:15 +0000

    Refactor cmake po macros
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@1958 69509848-2a73-49d5-a893-c4d8c934025a

--- cmake/GetText.cmake
+++ cmake/GetText.cmake
@@ -17,11 +17,50 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+function(GET_MO_DIR resultdir resultinstdir lang)
+  IF (APPLE)
+    SET(${resultdir} ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.app/Contents/Resources/${lang}.lproj PARENT_SCOPE)
+    SET(${resultinstdir} ${PROJECT_NAME}.app/Contents/Resources/${lang}.lproj PARENT_SCOPE)
+  ELSE()
+    SET(${resultdir} ${CMAKE_BINARY_DIR}/share/locale/${lang}/LC_MESSAGES PARENT_SCOPE)
+    SET(${resultinstdir} share/locale/${lang}/LC_MESSAGES PARENT_SCOPE)
+  ENDIF()
+endfunction()
+
+function(GETTEXT_UPDATE_POT target _potFile pattern)
+  file(GLOB_RECURSE SRC_FILES ${pattern})
+  set(_lst)
+  foreach(FILENAME ${SRC_FILES})
+    set(_lst "${_lst}${FILENAME}\n") 
+  endforeach(FILENAME)
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${_potFile}.src_lst "${_lst}")
+  ADD_CUSTOM_TARGET(${target} COMMAND ${GETTEXT_XGETTEXT_EXECUTABLE} -o ${_potFile} --files-from ${CMAKE_CURRENT_BINARY_DIR}/${_potFile}.src_lst ${ARGN})
+endfunction()
+
 function(GETTEXT_MERGE_PO target _potFile)
 	SET(_cmds)
 	FOREACH (_currentPoFile ${ARGN})
 		SET(_cmds ${_cmds} COMMAND ${GETTEXT_MSGMERGE_EXECUTABLE} --update --backup=none -s ${_currentPoFile} ${_potFile} -v )
 	ENDFOREACH (_currentPoFile )
 	ADD_CUSTOM_TARGET(${target} ${_cmds})
+endfunction()
+
+function(GETTEXT_INSTALL_PO basename)
+  SET(_moFiles)
+  SET(_addToAll)
+  FOREACH (_currentPoFile ${ARGN})
+    GET_FILENAME_COMPONENT(_lang ${_currentPoFile} NAME_WE)
+    GET_MO_DIR(_moDir _moInstDir ${_lang})
+    SET(_moFile ${_moDir}/${basename}.mo)
+    SET(_moFiles ${_moFiles} ${_moFile})
+    file(MAKE_DIRECTORY ${_moDir})
+
+    ADD_CUSTOM_COMMAND(OUTPUT ${_moFile} COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} -o ${_moFile} ${_currentPoFile} DEPENDS ${_currentPoFile})
+    IF (GETTEXT_FOUND)
+      INSTALL(FILES ${_moFile} DESTINATION ${_moInstDir})
+    ENDIF()
+  ENDFOREACH (_currentPoFile )
+
+  ADD_CUSTOM_TARGET(translations ALL DEPENDS ${_moFiles})
 endfunction()
 
