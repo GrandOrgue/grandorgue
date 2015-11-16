5af83fe558983f091ef96bef6690ee2f7cdb226d
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2015-11-16 21:54:13 +0000

    Add GOrgueArchiveFile
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@2004 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueArchiveFile.h
@@ -0,0 +1,60 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GORGUEARCHIVEFILE_H
+#define GORGUEARCHIVEFILE_H
+
+#include <wx/string.h>
+#include <vector>
+
+class GOrgueConfigReader;
+class GOrgueConfigWriter;
+class GOrgueSettings;
+
+class GOrgueArchiveFile
+{
+private:
+	wxString m_ID;
+	wxString m_Path;
+	wxString m_Name;
+	std::vector<wxString> m_Dependencies;
+	std::vector<wxString> m_DependencyTitles;
+
+public:
+	GOrgueArchiveFile(wxString id, wxString path, wxString name, const std::vector<wxString>& dependencies, const std::vector<wxString>& dependency_titles);
+	GOrgueArchiveFile(GOrgueConfigReader& cfg, wxString group);
+	virtual ~GOrgueArchiveFile();
+
+	void Update(const GOrgueArchiveFile& archive);
+
+	void Save(GOrgueConfigWriter& cfg, wxString group);
+
+	const wxString& GetID() const;
+	const wxString& GetPath() const;
+	const wxString& GetName() const;
+	const std::vector<wxString>& GetDependencies() const;
+	const std::vector<wxString>& GetDependencyTitles() const;
+
+	bool IsUsable(GOrgueSettings& settings);
+	bool IsComplete(GOrgueSettings& settings);
+};
+
+#endif
