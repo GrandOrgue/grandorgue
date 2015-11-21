d700f0f561b72cd16b64365276e98420402b00b8
Martin Koegler <martin.koegler@chello.at>
2015-11-21 20:48:41 +0000

    Add GOrgueArchive
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@2011 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOrgueArchive.cpp
@@ -0,0 +1,118 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "GOrgueArchive.h"
+
+#include "GOrgueArchiveIndex.h"
+#include "GOrgueArchiveReader.h"
+#include "GOrgueInvalidFile.h"
+#include <wx/intl.h>
+
+GOrgueArchive::GOrgueArchive(GOrgueSettings& settings) :
+	m_Settings(settings),
+	m_ID(),
+	m_Dependencies(),
+	m_Entries(),
+	m_Path()
+{
+}
+
+GOrgueArchive::~GOrgueArchive()
+{
+	Close();
+}
+
+bool GOrgueArchive::OpenArchive(const wxString& path)
+{
+	m_Entries.clear();
+	m_Path = path;
+	if (!m_File.Open(path, wxFile::read))
+	{
+		wxLogError(_("Failed to open '%s'"), path.c_str());
+		return false;
+	}
+	{
+		GOrgueArchiveIndex index(m_Settings, m_Path);
+		if (index.ReadIndex(m_ID, m_Entries))
+			return true;
+	}
+
+	GOrgueArchiveReader reader(m_File);
+	if (!reader.ListContent(m_ID, m_Entries))
+	{
+		wxLogError(_("Failed to parse '%s'"), path.c_str());
+		return false;
+	}
+
+	GOrgueArchiveIndex index(m_Settings, m_Path);
+	index.WriteIndex(m_ID, m_Entries);
+	return true;
+}
+
+void GOrgueArchive::Close()
+{
+	m_File.Close();
+	m_Entries.clear();
+}
+
+bool GOrgueArchive::containsFile(const wxString& name)
+{
+	for(unsigned i = 0; i < m_Entries.size(); i++)
+		if (m_Entries[i].name == name)
+			return true;
+	return false;
+}
+
+GOrgueFile* GOrgueArchive::OpenFile(const wxString& name)
+{
+	return new GOrgueInvalidFile(name);
+}
+
+size_t GOrgueArchive::ReadContent(void* buffer, size_t offset, size_t len)
+{
+	GOMutexLocker lock(m_Mutex);
+	ssize_t pos = m_File.Seek(offset);
+	if (pos != (ssize_t)offset)
+		return 0;
+	ssize_t l = m_File.Read(buffer, len);
+	if (l == wxInvalidOffset)
+		return 0;
+	return l;
+}
+
+const wxString& GOrgueArchive::GetArchiveID()
+{
+	return m_ID;
+}
+const wxString& GOrgueArchive::GetPath()
+{
+	return m_Path;
+}
+
+const std::vector<wxString>& GOrgueArchive::GetDependencies() const
+{
+	return m_Dependencies;
+}
+
+void GOrgueArchive::SetDependencies(const std::vector<wxString>& dependencies)
+{
+	m_Dependencies = dependencies;
+}
---
+++ src/grandorgue/GOrgueArchive.h
@@ -0,0 +1,64 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2015 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GORGUEARCHIVE_H
+#define GORGUEARCHIVE_H
+
+#include "GOLock.h"
+#include <wx/file.h>
+#include <wx/string.h>
+#include <vector>
+
+class GOrgueFile;
+class GOrgueSettings;
+typedef struct _GOArchiveEntry GOArchiveEntry;
+
+class GOrgueArchive
+{
+private:
+	GOMutex m_Mutex;
+	GOrgueSettings& m_Settings;
+	wxString m_ID;
+	std::vector<wxString> m_Dependencies;
+	std::vector<GOArchiveEntry> m_Entries;
+	wxFile m_File;
+	wxString m_Path;
+
+public:
+	GOrgueArchive(GOrgueSettings& settings);
+	~GOrgueArchive();
+
+	bool OpenArchive(const wxString& path);
+	void Close();
+
+	bool containsFile(const wxString& name);
+	GOrgueFile* OpenFile(const wxString& name);
+
+	size_t ReadContent(void* buffer, size_t offset, size_t len);
+
+	const wxString& GetArchiveID();
+	const wxString& GetPath();
+
+	const std::vector<wxString>& GetDependencies() const;
+	void SetDependencies(const std::vector<wxString>& dependencies);
+};
+
+#endif
