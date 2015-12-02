668af07f3e141f398b2fc865d989f3fa591ab578
Martin Koegler <martin.koegler@chello.at>
2015-12-02 07:48:46 +0000

    Use GetWood
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@2030 69509848-2a73-49d5-a893-c4d8c934025a

--- src/grandorgue/GOGUIHW1Background.cpp
+++ src/grandorgue/GOGUIHW1Background.cpp
@@ -21,12 +21,14 @@
 
 #include "GOGUIHW1Background.h"
 
+#include "GOrgueDC.h"
 #include "GOGUIDisplayMetrics.h"
 #include "GOGUILayoutEngine.h"
 #include "GOGUIPanel.h"
 
 GOGUIHW1Background::GOGUIHW1Background(GOGUIPanel* panel):
-	GOGUIControl(panel, NULL)
+	GOGUIControl(panel, NULL),
+	m_Images()
 {
 }
 
@@ -35,34 +37,52 @@ void GOGUIHW1Background::Init(GOrgueConfigReader& cfg, wxString group)
 	GOGUIControl::Init(cfg, group);
 }
 
+void GOGUIHW1Background::Layout()
+{
+	m_Images.clear();
+	wxRect rect;
+
+	rect = wxRect(0, 0, m_layout->GetCenterX(), m_metrics->GetScreenHeight());
+	m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopBackgroundImageNum())));
+	rect = wxRect(m_layout->GetCenterX() + m_layout->GetCenterWidth(), 0,
+		      m_metrics->GetScreenWidth() - (m_layout->GetCenterX() + m_layout->GetCenterWidth()), m_metrics->GetScreenHeight());
+	m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopBackgroundImageNum())));
+	rect = wxRect(m_layout->GetCenterX(), 0, m_layout->GetCenterWidth(), m_metrics->GetScreenHeight());
+	m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetConsoleBackgroundImageNum())));
+
+	if (m_metrics->HasPairDrawstopCols())
+	{
+		for (unsigned i = 0; i < (m_metrics->NumberOfDrawstopColsToDisplay() >> 2); i++)
+		{
+			rect = wxRect(i * (2 * m_metrics->GetDrawstopWidth() + 18) + m_layout->GetJambLeftX() - 5,
+				      m_layout->GetJambLeftRightY(), 2 * m_metrics->GetDrawstopWidth() + 10, m_layout->GetJambLeftRightHeight());
+			m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopInsetBackgroundImageNum())));
+			rect = wxRect(i * (2 * m_metrics->GetDrawstopWidth() + 18) + m_layout->GetJambRightX() - 5,
+				      m_layout->GetJambLeftRightY(), 2 * m_metrics->GetDrawstopWidth() + 10, m_layout->GetJambLeftRightHeight());
+			m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopInsetBackgroundImageNum())));
+		}
+	}
+
+	if (m_metrics->HasTrimAboveExtraRows())
+	{
+		rect = wxRect(m_layout->GetCenterX(), m_layout->GetCenterY(), m_layout->GetCenterWidth(), 8);
+		m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetKeyVertBackgroundImageNum())));
+	}
+
+	if (m_layout->GetJambTopHeight() + m_layout->GetPistonTopHeight())
+	{
+		rect = wxRect(m_layout->GetCenterX(), m_layout->GetJambTopY(), m_layout->GetCenterWidth(), m_layout->GetJambTopHeight() + m_layout->GetPistonTopHeight());
+		m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetKeyHorizBackgroundImageNum())));
+	}
+}
+
 void GOGUIHW1Background::PrepareDraw(double scale, GOrgueBitmap* background)
 {
 }
 
 void GOGUIHW1Background::Draw(GOrgueDC& dc)
 {
-	m_panel->TileWood(dc, m_metrics->GetDrawstopBackgroundImageNum(), 0, 0, m_layout->GetCenterX(), m_metrics->GetScreenHeight());
-	m_panel->TileWood(dc, m_metrics->GetDrawstopBackgroundImageNum(), m_layout->GetCenterX() + m_layout->GetCenterWidth(), 
-			  0, m_metrics->GetScreenWidth() - (m_layout->GetCenterX() + m_layout->GetCenterWidth()), m_metrics->GetScreenHeight());
-	m_panel->TileWood(dc, m_metrics->GetConsoleBackgroundImageNum(), m_layout->GetCenterX(), 0, m_layout->GetCenterWidth(), m_metrics->GetScreenHeight());
-
-	if (m_metrics->HasPairDrawstopCols())
-	{
-		for (unsigned i = 0; i < (m_metrics->NumberOfDrawstopColsToDisplay() >> 2); i++)
-		{
-			m_panel->TileWood(dc, m_metrics->GetDrawstopInsetBackgroundImageNum(), i * (2 * m_metrics->GetDrawstopWidth() + 18) + m_layout->GetJambLeftX() - 5,
-				m_layout->GetJambLeftRightY(), 2 * m_metrics->GetDrawstopWidth() + 10, m_layout->GetJambLeftRightHeight());
-			m_panel->TileWood(dc, m_metrics->GetDrawstopInsetBackgroundImageNum(), i * (2 * m_metrics->GetDrawstopWidth() + 18) + m_layout->GetJambRightX() - 5, 
-					  m_layout->GetJambLeftRightY(), 2 * m_metrics->GetDrawstopWidth() + 10, m_layout->GetJambLeftRightHeight());
-		}
-	}
-
-	if (m_metrics->HasTrimAboveExtraRows())
-		m_panel->TileWood(dc, m_metrics->GetKeyVertBackgroundImageNum(), m_layout->GetCenterX(), m_layout->GetCenterY(), m_layout->GetCenterWidth(), 8);
-
-	if (m_layout->GetJambTopHeight() + m_layout->GetPistonTopHeight())
-		m_panel->TileWood(dc, m_metrics->GetKeyHorizBackgroundImageNum(), m_layout->GetCenterX(), m_layout->GetJambTopY(), 
-			 m_layout->GetCenterWidth(), m_layout->GetJambTopHeight() + m_layout->GetPistonTopHeight());
-
+	for(unsigned i = 0; i < m_Images.size(); i++)
+		dc.TileBitmap(m_Images[i].bmp, m_Images[i].rect, 0, 0);
 	GOGUIControl::Draw(dc);
 }
--- src/grandorgue/GOGUIHW1Background.h
+++ src/grandorgue/GOGUIHW1Background.h
@@ -24,12 +24,32 @@
 
 #include "GOGUIControl.h"
 
+#include "GOrgueBitmap.h"
+#include <vector>
+
 class GOGUIHW1Background : public GOGUIControl
 {
+	class GOBackgroundImage
+	{
+	public:
+
+	wxRect rect;
+	GOrgueBitmap bmp;
+
+	GOBackgroundImage(wxRect Rect, const GOrgueBitmap& Bmp) :
+		rect(Rect),
+		bmp(Bmp)
+		{
+		}
+	};
+private:
+	std::vector<GOBackgroundImage> m_Images;
+
 public:
 	GOGUIHW1Background(GOGUIPanel* panel);
 
 	void Init(GOrgueConfigReader& cfg, wxString group);
+	void Layout();
 
 	void PrepareDraw(double scale, GOrgueBitmap* background);
 	void Draw(GOrgueDC& dc);
