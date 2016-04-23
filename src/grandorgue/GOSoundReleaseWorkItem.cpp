715e7d676dd68ab2df996fc6a4fa52f234f7a75f
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2016-04-23 12:54:06 +0000

    Add missing files
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@2106 69509848-2a73-49d5-a893-c4d8c934025a

---
+++ src/grandorgue/GOSoundReleaseWorkItem.cpp
@@ -0,0 +1,72 @@
+/*
+ * GrandOrgue - free pipe organ simulator
+ *
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2016 GrandOrgue contributors (see AUTHORS)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "GOSoundReleaseWorkItem.h"
+
+#include "GOSoundEngine.h"
+
+GOSoundReleaseWorkItem::GOSoundReleaseWorkItem(GOSoundEngine& sound_engine) :
+	m_engine(sound_engine)
+{
+}
+
+unsigned GOSoundReleaseWorkItem::GetGroup()
+{
+	return RELEASE;
+}
+
+unsigned GOSoundReleaseWorkItem::GetCost()
+{
+	return 0;
+}
+
+bool GOSoundReleaseWorkItem::GetRepeat()
+{
+	return true;
+}
+
+void GOSoundReleaseWorkItem::Clear()
+{
+	m_List.Clear();
+}
+
+void GOSoundReleaseWorkItem::Reset()
+{
+}
+
+void GOSoundReleaseWorkItem::Add(GO_SAMPLER* sampler)
+{
+	m_List.Put(sampler);
+}
+
+void GOSoundReleaseWorkItem::Run()
+{
+	GO_SAMPLER* sampler;
+	while((sampler = m_List.Get()))
+	{
+		m_engine.ProcessRelease(sampler);
+	}
+}
+
+void GOSoundReleaseWorkItem::Exec()
+{
+	Run();
+}
