2b9e327bc93229dc6f0ae981e7db94fde0916f46
e9925248 <e9925248@69509848-2a73-49d5-a893-c4d8c934025a>
2016-08-04 07:47:59 +0000

    Fix OS X bundle
    git-svn-id: https://svn.code.sf.net/p/ourorgan/svn/trunk@2143 69509848-2a73-49d5-a893-c4d8c934025a

--- cmake/FixupBundle.cmake
+++ cmake/FixupBundle.cmake
@@ -18,6 +18,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 include(BundleUtilities)
+include(CMakeParseArguments)
+include(${CMAKE_CURRENT_LIST_DIR}/BundleUtilities.cmake)
 
 function(verify_app)
 endfunction()
