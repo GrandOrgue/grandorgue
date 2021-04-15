d18610a6fe6aee81ded0b4d8166e554ad5c8a890
Oleg Samarin <osamarin@openwaygroup.com>
2021-04-15 23:06:34 +0300

    Version number from git tags

--- build-scripts/for-linux/build-on-linux.sh
+++ build-scripts/for-linux/build-on-linux.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# $1 - Revision
-# $2 - Go source Dir. If not set then relative to the script dir
+# $1 - Version
+# $2 - Build version
+# $3 - Go source Dir. If not set then relative to the script dir
 
 set -e
 
-if [[ -n "$1" ]]; then
-	REVISION_PRM=-DVERSION_REVISION=$1
-else
-	REVISION_PRM=
-fi
+source $(dirname $0)/../set-ver-prms.sh "$1" "$2"
 
-if [[ -n "$2" ]]; then
-	SRC_DIR=$2
+if [[ -n "$3" ]]; then
+	SRC_DIR=$3
 else
 	SRC_DIR=$(readlink -f $(dirname $0)/../..)
 fi
@@ -25,8 +22,9 @@ pushd build-linux
 rm -rf *
 export LANG=C
 
-GO_PRMS="-DCMAKE_BUILD_TYPE=Release $REVISION_PRM"
-cmake -G "Unix Makefiles" $GO_PRMS $SRC_DIR
+GO_PRMS="-DCMAKE_BUILD_TYPE=Release $CMAKE_VERSION_PRMS"
+echo "cmake -G \"Unix Makefiles\" $GO_PRMS . $SRC_DIR"
+cmake -G "Unix Makefiles" $GO_PRMS . $SRC_DIR
 make -k $PARALLEL_PRMS VERBOSE=1 package
 
 popd
