f44c19fd82531229d4b6d4520624288c573a6483
Oleg Samarin <osamarin@openwaygroup.com>
2021-05-03 20:19:34 +0300

    Fixed crash on OSX 11.x

--- src/grandorgue/GOGUIHW1Background.cpp
+++ src/grandorgue/GOGUIHW1Background.cpp
@@ -37,18 +37,25 @@ void GOGUIHW1Background::Init(GOrgueConfigReader& cfg, wxString group)
 	GOGUIControl::Init(cfg, group);
 }
 
+bool is_rect_valid(wxRect& rect) {
+  return rect.width > 0 && rect.height > 0;
+}
+
 void GOGUIHW1Background::Layout()
 {
 	m_Images.clear();
 	wxRect rect;
 
 	rect = wxRect(0, 0, m_layout->GetCenterX(), m_metrics->GetScreenHeight());
-	m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopBackgroundImageNum())));
+	if (is_rect_valid(rect))
+	  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopBackgroundImageNum())));
 	rect = wxRect(m_layout->GetCenterX() + m_layout->GetCenterWidth(), 0,
 		      m_metrics->GetScreenWidth() - (m_layout->GetCenterX() + m_layout->GetCenterWidth()), m_metrics->GetScreenHeight());
-	m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopBackgroundImageNum())));
+	if (is_rect_valid(rect))
+	  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopBackgroundImageNum())));
 	rect = wxRect(m_layout->GetCenterX(), 0, m_layout->GetCenterWidth(), m_metrics->GetScreenHeight());
-	m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetConsoleBackgroundImageNum())));
+	if (is_rect_valid(rect))
+	  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetConsoleBackgroundImageNum())));
 
 	if (m_metrics->HasPairDrawstopCols())
 	{
@@ -56,23 +63,27 @@ void GOGUIHW1Background::Layout()
 		{
 			rect = wxRect(i * (2 * m_metrics->GetDrawstopWidth() + 18) + m_layout->GetJambLeftX() - 5,
 				      m_layout->GetJambLeftRightY(), 2 * m_metrics->GetDrawstopWidth() + 10, m_layout->GetJambLeftRightHeight());
-			m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopInsetBackgroundImageNum())));
+			if (is_rect_valid(rect))
+			  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopInsetBackgroundImageNum())));
 			rect = wxRect(i * (2 * m_metrics->GetDrawstopWidth() + 18) + m_layout->GetJambRightX() - 5,
 				      m_layout->GetJambLeftRightY(), 2 * m_metrics->GetDrawstopWidth() + 10, m_layout->GetJambLeftRightHeight());
-			m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopInsetBackgroundImageNum())));
+			if (is_rect_valid(rect))
+			  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetDrawstopInsetBackgroundImageNum())));
 		}
 	}
 
 	if (m_metrics->HasTrimAboveExtraRows())
 	{
 		rect = wxRect(m_layout->GetCenterX(), m_layout->GetCenterY(), m_layout->GetCenterWidth(), 8);
-		m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetKeyVertBackgroundImageNum())));
+		if (is_rect_valid(rect))
+		  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetKeyVertBackgroundImageNum())));
 	}
 
 	if (m_layout->GetJambTopHeight() + m_layout->GetPistonTopHeight())
 	{
 		rect = wxRect(m_layout->GetCenterX(), m_layout->GetJambTopY(), m_layout->GetCenterWidth(), m_layout->GetJambTopHeight() + m_layout->GetPistonTopHeight());
-		m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetKeyHorizBackgroundImageNum())));
+		if (is_rect_valid(rect))
+		  m_Images.push_back(GOBackgroundImage(rect, m_panel->GetWood(m_metrics->GetKeyHorizBackgroundImageNum())));
 	}
 }
 
