f2ae5db2752bfb79d2310791ef8e9a14b0f30635
Denis Roussel <denis.roussel@acsone.eu>
2021-09-27 11:24:37 +0200

    [IMP] Add 'or later' to license headers

--- src/grandorgue/GOSoundScheduler.h
+++ src/grandorgue/GOSoundScheduler.h
@@ -1,7 +1,7 @@
 /*
 * Copyright 2006 Milan Digital Audio LLC
 * Copyright 2009-2019 GrandOrgue contributors (see AUTHORS)
-* License GPL-2.0 (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+* License GPL-2.0 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
 */
 
 #ifndef GORGUESOUNDSCHEDULER_H
