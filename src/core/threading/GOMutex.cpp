0a62cb87f3aea7a3ab008ba04291a60f65d025b0
Oleg Samarin <osamarin@openwaygroup.com>
2021-10-09 22:28:00 +0300

    Added copyright headers https://github.com/GrandOrgue/grandorgue/issues/762

--- src/core/threading/GOMutex.cpp
+++ src/core/threading/GOMutex.cpp
@@ -1,3 +1,8 @@
+/*
+* Copyright 2006 Milan Digital Audio LLC
+* Copyright 2009-2021 GrandOrgue contributors (see AUTHORS)
+* License GPL-2.0 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+*/
 #include "GOMutex.h"
 
 #define GO_PRINTCONTENTION 0
