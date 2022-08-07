2d25a5b43a721189c9894bfbab736bf975804ea4
Oleg Samarin <osamarin68@gmail.com>
2022-08-07 18:11:11 +0300

    Fixed packaging for OSx https://github.com/GrandOrgue/grandorgue/issues/1135

--- build-scripts/for-osx/prepare-osx.sh
+++ build-scripts/for-osx/prepare-osx.sh
@@ -3,3 +3,11 @@
 set -e
 brew install gettext jack docbook-xsl wxmac cmake pkg-config fftw wavpack imagemagick
 brew link gettext --force
+
+# A workaround of https://gitlab.kitware.com/cmake/cmake/-/issues/23826
+for F in $(grep -l '(, weak)\?'  /usr/local/Cellar/cmake/*/share/cmake/Modules/GetPrerequisites.cmake); do
+  echo Patching $F...
+  # OSx sed does not support the -i option
+  sed -e 's/(, weak)\?/(, (weak|reexport))?/' $F >${F}.new
+  mv ${F}.new $F
+done
