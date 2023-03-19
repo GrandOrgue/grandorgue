3506eeb42aa0f852a73a85c64b736f1fe8d08d8b
Oleg Samarin <osamarin68@gmail.com>
2023-03-19 16:13:46 +0300

    Removed old cmake files (#1436)

--- cmake/FixupBundle.cmake
+++ cmake/FixupBundle.cmake
@@ -18,8 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 include(BundleUtilities)
-include(CMakeParseArguments)
-include(${CMAKE_CURRENT_LIST_DIR}/BundleUtilities.cmake)
 
 function(verify_app)
 endfunction()
