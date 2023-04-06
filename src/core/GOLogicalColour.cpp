f3b4911dcdcd0c25a7e7a39725ac34cf25ac8ce6
Oleg Samarin <osamarin68@gmail.com>
2023-04-06 22:46:57 +0300

    Added capability of using GOLogicalColour constants outside the config
    (cherry picked from commit f9df0bce6e1795781262a5f894def0660f6fbb65)

---
+++ src/core/GOLogicalColour.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * License GPL-2.0 or later
+ * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+ */
+
+#include "GOLogicalColour.h"
+
+const GOLogicalColour GOLogicalColour::BLACK
+  = GOLogicalColour(0x00, 0x00, 0x00);
+const GOLogicalColour GOLogicalColour::BLUE = GOLogicalColour(0x00, 0x00, 0xFF);
+const GOLogicalColour GOLogicalColour::DARK_BLUE
+  = GOLogicalColour(0x00, 0x00, 0x80);
+const GOLogicalColour GOLogicalColour::GREEN
+  = GOLogicalColour(0x00, 0xFF, 0x00);
+const GOLogicalColour GOLogicalColour::DARK_GREEN
+  = GOLogicalColour(0x00, 0x80, 0x00);
+const GOLogicalColour GOLogicalColour::CYAN = GOLogicalColour(0x00, 0xFF, 0xFF);
+const GOLogicalColour GOLogicalColour::DARK_CYAN
+  = GOLogicalColour(0x00, 0x80, 0x80);
+const GOLogicalColour GOLogicalColour::RED = GOLogicalColour(0xFF, 0x00, 0x00);
+const GOLogicalColour GOLogicalColour::DARK_RED
+  = GOLogicalColour(0x80, 0x00, 0x00);
+const GOLogicalColour GOLogicalColour::MAGENTA
+  = GOLogicalColour(0xFF, 0x00, 0xFF);
+const GOLogicalColour GOLogicalColour::DARK_MAGENTA
+  = GOLogicalColour(0x80, 0x00, 0x80);
+const GOLogicalColour GOLogicalColour::YELLOW
+  = GOLogicalColour(0xFF, 0xFF, 0x00);
+const GOLogicalColour GOLogicalColour::DARK_YELLOW
+  = GOLogicalColour(0x80, 0x80, 0x00);
+const GOLogicalColour GOLogicalColour::LIGHT_GREY
+  = GOLogicalColour(0xC0, 0xC0, 0xC0);
+const GOLogicalColour GOLogicalColour::DARK_GREY
+  = GOLogicalColour(0x80, 0x80, 0x80);
+const GOLogicalColour GOLogicalColour::WHITE
+  = GOLogicalColour(0xFF, 0xFF, 0xFF);
+const GOLogicalColour GOLogicalColour::BROWN
+  = GOLogicalColour(0xA5, 0x2A, 0x2A);
