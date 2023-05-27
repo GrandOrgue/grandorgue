f3ba117c38f3ab4ebfe23c2797ba66baa7c6f511
Oleg Samarin <osamarin68@gmail.com>
2023-05-27 22:02:58 +0300

    Calculate path to yaml-cpp-static

--- build-scripts/for-linux/build-on-linux.sh
+++ build-scripts/for-linux/build-on-linux.sh
@@ -4,6 +4,7 @@
 # $2 - Build version
 # $3 - Go source Dir. If not set then relative to the script dir
 # $4 - package suffix: empty or wx30
+# $5 - target deb architecture. Default - current
 
 set -e
 
@@ -16,6 +17,7 @@ else
 fi
 
 PACKAGE_SUFFIX=$4
+TARGET_ARCH=${5:$(dpkg --print-architecture)}
 
 PARALLEL_PRMS="-j$(nproc)"
 
@@ -26,6 +28,15 @@ rm -rf *
 export LANG=C
 
 GO_PRMS="-DCMAKE_BUILD_TYPE=Release $CMAKE_VERSION_PRMS -DCMAKE_PACKAGE_SUFFIX=$PACKAGE_SUFFIX"
+
+#specify the yaml-cpp-static path
+MULTIARCH=$(dpkg-architecture -q DEB_TARGET_MULTIARCH -A $TARGET_ARCH)
+if [[ -d /usr/lib/$MULTIARCH/cmake/yaml-cpp-static ]]; then
+  GO_PRMS="$GO_PRMS -Dyaml-cpp_DIR=/usr/lib/$MULTIARCH/cmake/yaml-cpp-static"
+elif [[ -d /usr/lib64/cmake/yaml-cpp-static ]]; then
+  GO_PRMS="$GO_PRMS -Dyaml-cpp_DIR=/usr/lib64/cmake/yaml-cpp-static"
+fi
+
 echo "cmake -G \"Unix Makefiles\" $GO_PRMS . $SRC_DIR"
 cmake -G "Unix Makefiles" $GO_PRMS . $SRC_DIR
 make -k $PARALLEL_PRMS VERBOSE=1 package
