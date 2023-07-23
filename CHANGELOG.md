468ce167ed2895d5405adc9d161cc2954accffdb
Oleg Samarin <osamarin68@gmail.com>
2023-07-23 14:24:25 +0300

    Fixed size of the Organ Settings Dialog https://github.com/GrandOrgue/grandorgue/issues/1415 (#1594)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+- Fixed size of the Organ Settings Dialog https://github.com/GrandOrgue/grandorgue/issues/1415
 - Fixed an incorrect dialog window icon
 - Fixed absence of the Help button on the Organ Setting dialog https://github.com/GrandOrgue/grandorgue/issues/1416
 - Fixed displaying buttons if the manual is not visible https://github.com/GrandOrgue/grandorgue/issues/1566
--- src/grandorgue/dialogs/common/GOSimpleDialog.cpp
+++ src/grandorgue/dialogs/common/GOSimpleDialog.cpp
@@ -10,11 +10,26 @@
 #include <wx/sizer.h>
 #include <wx/statline.h>
 
-void GOSimpleDialog::LayoutWithInnerSizer(wxSizer *pInnerSizer) {
-  wxBoxSizer *const pTopSizer = new wxBoxSizer(wxVERTICAL);
+GOSimpleDialog::GOSimpleDialog(
+  wxWindow *parent,
+  const wxString &name,  // not translated
+  const wxString &title, // translated
+  long addStyle,
+  long buttonFlags)
+  : GODialog(name) {
+  // This call is necessary for the dialog not to cover the help window
+  if (Create(parent, title, addStyle, buttonFlags)) {
+    wxBoxSizer *const pTopSizer = new wxBoxSizer(wxVERTICAL);
 
-  pTopSizer->Add(pInnerSizer, 1, wxGROW | wxEXPAND | wxALL, 5);
-  pTopSizer->Add(new wxStaticLine(this), 0, wxEXPAND | wxALL, 5);
-  pTopSizer->Add(GetButtonSizer(), 0, wxEXPAND | wxALL, 5);
-  SetSizerAndFit(pTopSizer);
+    p_ContentSizer = new wxBoxSizer(wxVERTICAL);
+    pTopSizer->Add(p_ContentSizer, 1, wxGROW | wxEXPAND | wxALL, 5);
+    pTopSizer->Add(new wxStaticLine(this), 0, wxEXPAND | wxALL, 5);
+    pTopSizer->Add(GetButtonSizer(), 0, wxEXPAND | wxALL, 5);
+    SetSizerAndFit(pTopSizer);
+  }
+}
+
+void GOSimpleDialog::LayoutWithInnerSizer(wxSizer *pInnerSizer) {
+  p_ContentSizer->Add(pInnerSizer, 1, wxGROW | wxEXPAND);
+  GetSizer()->SetSizeHints(this);
 }
