e8830550dcd7a53e1be6b49f4327fdd4d6697525
Denis Roussel <denis.roussel@acsone.eu>
2023-08-03 22:11:22 +0200

    [CHG] Change to easier tests

---
+++ src/tests/GOTestLib.cpp
@@ -0,0 +1,13 @@
+#include "config/GOConfig.h"
+#include <iostream>
+#include <wx/app.h>
+#include <wx/filename.h>
+#include <wx/image.h>
+#include <wx/stopwatch.h>
+
+int main() {
+
+  std::cout << "Tests started!" << std::endl;
+  std::cout << "Tests (0) succeeded!" << std::endl;
+  return 0; // You can put a 1 here to see later that it would generate an error
+}
