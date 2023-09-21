f362052e72c067e4e257cd29a81f83236af5af68
Oleg Samarin <osamarin68@gmail.com>
2023-09-21 22:57:33 +0300

    Extracted enums to separate header files

---
+++ src/core/midi/GOMidiReceiverType.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * License GPL-2.0 or later
+ * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+ */
+
+#ifndef GOMIDIRECEIVERTYPE_H
+#define GOMIDIRECEIVERTYPE_H
+
+enum GOMidiReceiverType {
+  MIDI_RECV_DRAWSTOP,
+  MIDI_RECV_BUTTON,
+  MIDI_RECV_ENCLOSURE,
+  MIDI_RECV_MANUAL,
+  MIDI_RECV_SETTER,
+  MIDI_RECV_ORGAN,
+};
+
+#endif /* GOMIDIRECEIVERTYPE_H */
