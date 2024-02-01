ee7aec174223c96aa069b91f9b008c455e840e84
Oleg Samarin <osamarin68@gmail.com>
2024-02-01 10:32:55 +0300

    Simplified code of GOPipeConfig and GOPipeConfigNode (#1782)

--- src/grandorgue/model/pipe-config/GOPipeConfig.h
+++ src/grandorgue/model/pipe-config/GOPipeConfig.h
@@ -10,10 +10,12 @@
 
 #include <wx/string.h>
 
+#include "GOPipeConfigListener.h"
+#include "GOPipeUpdateCallback.h"
+
 class GOConfigReader;
 class GOConfigWriter;
 class GOPipeConfigListener;
-class GOPipeUpdateCallback;
 
 class GOPipeConfig {
 private:
@@ -22,29 +24,56 @@ private:
   wxString m_Group;
   wxString m_NamePrefix;
   wxString m_AudioGroup;
-  float m_Amplitude;
   float m_DefaultAmplitude;
+  float m_Amplitude;
   float m_DefaultGain;
+  float m_Gain;
   // ODF pitch tuning offset for using withot auto-tuning
   float m_PitchTuning;
   // ODF pitch tuning offset for using with auto-tuning
   float m_PitchCorrection;
-  float m_Gain;
   float m_ManualTuning;
   float m_AutoTuningCorrection;
-  unsigned m_Delay;
   unsigned m_DefaultDelay;
+  unsigned m_Delay;
+  unsigned m_ReleaseTail; // the max release length in ms
   int m_BitsPerSample;
-  int m_Compress;
   int m_Channels;
   int m_LoopLoad;
+  int m_Compress;
   int m_AttackLoad;
   int m_ReleaseLoad;
   int m_IgnorePitch;
-  unsigned m_ReleaseTail; // the max release length in ms
 
   void ReadTuning(
     GOConfigReader &cfg, const wxString &group, const wxString &prefix);
+
+  /* two generic setters SetSmallMember and SetLargeMember differ only in
+   * passing values */
+
+#define SET_MEMBER_BODY(value, member, callbackFun)                            \
+  member = value;                                                              \
+  if (callbackFun)                                                             \
+    (m_Callback->*callbackFun)();                                              \
+  r_listener.NotifyPipeConfigModified();
+
+  template <typename T>
+  void SetSmallMember(
+    const T value,
+    T &member,
+    void (GOPipeUpdateCallback::*callbackFun)() = nullptr) {
+    SET_MEMBER_BODY(value, member, callbackFun)
+  }
+
+  template <typename T>
+  void SetLargeMember(
+    const T &value,
+    T &member,
+    void (GOPipeUpdateCallback::*callbackFun)() = nullptr) {
+    SET_MEMBER_BODY(value, member, callbackFun)
+  }
+
+  void SetPitchMember(float cents, float &member);
 
 public:
   GOPipeConfig(GOPipeConfigListener &listener, GOPipeUpdateCallback *callback);
@@ -53,54 +82,66 @@ public:
   void Load(GOConfigReader &cfg, const wxString &group, const wxString &prefix);
   void Save(GOConfigWriter &cfg);
 
-  GOPipeUpdateCallback *GetCallback();
+  GOPipeUpdateCallback *GetCallback() const { return m_Callback; }
 
-  float GetAmplitude();
-  float GetDefaultAmplitude();
-  void SetAmplitude(float amp);
+  const wxString &GetAudioGroup() const { return m_AudioGroup; }
+  void SetAudioGroup(const wxString &str) {
+    SetLargeMember(str, m_AudioGroup, &GOPipeUpdateCallback::UpdateAudioGroup);
+  }
 
-  float GetGain();
-  float GetDefaultGain();
-  void SetGain(float gain);
+  float GetDefaultAmplitude() const { return m_DefaultAmplitude; }
+  float GetAmplitude() const { return m_Amplitude; }
+  void SetAmplitude(float amp) {
+    SetSmallMember(amp, m_Amplitude, &GOPipeUpdateCallback::UpdateAmplitude);
+  }
+
+  float GetDefaultGain() const { return m_DefaultGain; }
+  float GetGain() const { return m_Gain; }
+  void SetGain(float gain) {
+    SetSmallMember(gain, m_Gain, &GOPipeUpdateCallback::UpdateAmplitude);
+  }
 
   float GetPitchTuning() const { return m_PitchTuning; }
   float GetPitchCorrection() const { return m_PitchCorrection; }
+
   float GetManualTuning() const { return m_ManualTuning; }
-  void SetManualTuning(float cent);
+  void SetManualTuning(float cents) { SetPitchMember(cents, m_ManualTuning); }
+
   float GetAutoTuningCorrection() const { return m_AutoTuningCorrection; }
-  void SetAutoTuningCorrection(float cent);
+  void SetAutoTuningCorrection(float cents) {
+    SetPitchMember(cents, m_AutoTuningCorrection);
+  }
 
   unsigned GetDefaultDelay() const { return m_DefaultDelay; }
-
   unsigned GetDelay() const { return m_Delay; }
-  void SetDelay(unsigned delay);
-
-  const wxString &GetAudioGroup() const { return m_AudioGroup; }
-  void SetAudioGroup(const wxString &str);
-
-  int GetBitsPerSample() const { return m_BitsPerSample; }
-  void SetBitsPerSample(int value);
-
-  int GetCompress() const { return m_Compress; }
-  void SetCompress(int value);
-
-  int GetChannels() const { return m_Channels; }
-  void SetChannels(int value);
-
-  int GetLoopLoad() const { return m_LoopLoad; }
-  void SetLoopLoad(int value);
-
-  int GetAttackLoad() const { return m_AttackLoad; }
-  void SetAttackLoad(int value);
-
-  int GetReleaseLoad() const { return m_ReleaseLoad; }
-  void SetReleaseLoad(int value);
-
-  int IsIgnorePitch() const { return m_IgnorePitch; }
-  void SetIgnorePitch(int value);
+  void SetDelay(unsigned delay) { SetSmallMember(delay, m_Delay); }
 
   unsigned GetReleaseTail() const { return m_ReleaseTail; }
-  void SetReleaseTail(unsigned releaseTail);
+  void SetReleaseTail(unsigned releaseTail) {
+    SetSmallMember(
+      releaseTail, m_ReleaseTail, &GOPipeUpdateCallback::UpdateReleaseTail);
+  }
+
+  int GetBitsPerSample() const { return m_BitsPerSample; }
+  void SetBitsPerSample(int value) { SetSmallMember(value, m_BitsPerSample); }
+
+  int GetChannels() const { return m_Channels; }
+  void SetChannels(int value) { SetSmallMember(value, m_Channels); }
+
+  int GetLoopLoad() const { return m_LoopLoad; }
+  void SetLoopLoad(int value) { SetSmallMember(value, m_LoopLoad); }
+
+  int GetCompress() const { return m_Compress; }
+  void SetCompress(int value) { SetSmallMember(value, m_Compress); }
+
+  int GetAttackLoad() const { return m_AttackLoad; }
+  void SetAttackLoad(int value) { SetSmallMember(value, m_AttackLoad); }
+
+  int GetReleaseLoad() const { return m_ReleaseLoad; }
+  void SetReleaseLoad(int value) { SetSmallMember(value, m_ReleaseLoad); }
+
+  int IsIgnorePitch() const { return m_IgnorePitch; }
+  void SetIgnorePitch(int value) { SetSmallMember(value, m_IgnorePitch); }
 };
 
 #endif
