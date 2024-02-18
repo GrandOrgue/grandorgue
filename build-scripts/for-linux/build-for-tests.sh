cd23204f21a0caddb3d18fbd01dd07fd4e35f088
Denis Roussel <denis.roussel@acsone.eu>
2024-02-18 14:57:01 +0100

    [IMP][TESTS] Improve testing flow

--- build-scripts/for-linux/build-for-tests.sh
+++ build-scripts/for-linux/build-for-tests.sh
@@ -5,9 +5,12 @@
 
 set -e
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
-  libboost-stacktrace-dev git gcovr
+PARALLEL_PRMS="-j$(nproc)"
 
+# Install requirements for tests infrastructure
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git gcovr
+
+# Install cpptrace from sources
 mkdir cpptrace
 cd cpptrace
 git clone https://github.com/jeremy-rifkin/cpptrace.git .
@@ -15,33 +18,25 @@ git checkout v0.4.0
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j$PARALELL_PRMS
 sudo make install
 cd ../..
 
 DIR=$(dirname $0)
-# source $DIR/../set-ver-prms.sh "$1" "$2"
 
 SRC_DIR=$(readlink -f $(dirname $0)/../..)
 
-
-# PACKAGE_SUFFIX=$4
 TARGET_ARCH=${2:$(dpkg --print-architecture)}
 
-PARALLEL_PRMS="-j$(nproc)"
-
-mkdir -p build/linux
-pushd build/linux
-
-rm -rf *
 export LANG=C
 
-GO_PRMS="-DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBOOST_ENABLE_ASSERT_DEBUG_HANDLER=ON -DBOOST_STACKTRACE_USE_ADDR2LINE=ON \
+# Define BUILD_TESTING=ON
+GO_PRMS="-DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON \
   $CMAKE_VERSION_PRMS \
   $($DIR/cmake-prm-yaml-cpp.bash $TARGET_ARCH)"
 
+# Build simply GrandOrgue without packaging
 echo "cmake -G \"Unix Makefiles\" $GO_PRMS . $SRC_DIR"
 cmake -G "Unix Makefiles" $GO_PRMS . $SRC_DIR
-make -k $PARALLEL_PRMS VERBOSE=1 package
-
-popd
+echo "cmake --build . --parallel $PARALLEL_PRMS"
+cmake --build . --parallel $PARALLEL_PRMS
