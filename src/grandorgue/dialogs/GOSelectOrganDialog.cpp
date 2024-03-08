0ff3ab31d4121d9ccc3d19557345579bcceeae49
Oleg Samarin <osamarin68@gmail.com>
2024-03-08 23:02:47 +0300

    Changed displaying organ path in the Organ selection dialog

--- src/grandorgue/dialogs/GOSelectOrganDialog.cpp
+++ src/grandorgue/dialogs/GOSelectOrganDialog.cpp
@@ -7,12 +7,13 @@
 
 #include "GOSelectOrganDialog.h"
 
-#include <wx/listctrl.h>
 #include <wx/msgdlg.h>
 #include <wx/sizer.h>
 
 #include "archive/GOArchiveFile.h"
 #include "config/GOConfig.h"
+#include "size/GOAdditionalSizeKeeperProxy.h"
+#include "wxcontrols/GOGrid.h"
 
 #include "GOOrgan.h"
 #include "GOOrganList.h"
@@ -20,8 +21,11 @@
 enum { ID_ORGANS = 200 };
 
 BEGIN_EVENT_TABLE(GOSelectOrganDialog, GOSimpleDialog)
-EVT_LIST_ITEM_ACTIVATED(ID_ORGANS, GOSelectOrganDialog::OnDoubleClick)
+EVT_GRID_CMD_SELECT_CELL(ID_ORGANS, GOSelectOrganDialog::OnSelectCell)
+EVT_GRID_CMD_CELL_LEFT_DCLICK(ID_ORGANS, GOSelectOrganDialog::OnDoubleClick)
 END_EVENT_TABLE()
+
+enum { GRID_COL_NAME = 0, GRID_COL_PACKAGE, GRID_COL_PATH, GRID_N_COLS };
 
 GOSelectOrganDialog::GOSelectOrganDialog(wxWindow *parent, GOConfig &config)
   : GOSimpleDialog(
@@ -33,61 +37,99 @@ GOSelectOrganDialog::GOSelectOrganDialog(wxWindow *parent, GOConfig &config)
     wxDIALOG_NO_PARENT),
     r_OrganList(config) {
   wxBoxSizer *topSizer = new wxBoxSizer(wxVERTICAL);
-  topSizer->AddSpacer(5);
 
-  m_Organs = new wxListView(
-    this,
-    ID_ORGANS,
-    wxDefaultPosition,
-    wxDefaultSize,
-    wxLC_REPORT | wxLC_SINGLE_SEL | wxLC_HRULES | wxLC_VRULES);
-  m_Organs->InsertColumn(0, _("Church"));
-  m_Organs->InsertColumn(1, _("Organ package"));
-  m_Organs->InsertColumn(2, _("Organ path"));
-  topSizer->Add(m_Organs, 1, wxEXPAND | wxALL, 5);
+  m_GridOrgans
+    = new GOGrid(this, ID_ORGANS, wxDefaultPosition, wxSize(100, 40));
+  m_GridOrgans->CreateGrid(0, 3, wxGrid::wxGridSelectRows);
+  m_GridOrgans->HideRowLabels();
+  m_GridOrgans->EnableEditing(false);
+  m_GridOrgans->SetColLabelValue(GRID_COL_NAME, _("Name"));
+  m_GridOrgans->SetColLabelValue(GRID_COL_PACKAGE, _("Organ package"));
+  m_GridOrgans->SetColLabelValue(GRID_COL_PATH, _("Organ path"));
+  m_GridOrgans->SetColSize(GRID_COL_NAME, 150);
+  m_GridOrgans->SetColSize(GRID_COL_PACKAGE, 150);
+  m_GridOrgans->SetColSize(GRID_COL_PATH, 300);
+  m_GridOrgans->SetColumnRightVisible(GRID_COL_PATH, true);
+  topSizer->Add(m_GridOrgans, 1, wxEXPAND | wxALL, 5);
 
-  m_Organs->SetColumnWidth(0, 150);
-  m_Organs->SetColumnWidth(1, 150);
-  m_Organs->SetColumnWidth(2, 300);
-
-  topSizer->AddSpacer(5);
   LayoutWithInnerSizer(topSizer);
 }
 
+const wxString WX_GRID_ORGANS = wxT("GridOrgans");
+
+void GOSelectOrganDialog::ApplyAdditionalSizes(
+  const GOAdditionalSizeKeeper &sizeKeeper) {
+  GOAdditionalSizeKeeperProxy proxyGridOrgans(
+    const_cast<GOAdditionalSizeKeeper &>(sizeKeeper), WX_GRID_ORGANS);
+
+  m_GridOrgans->ApplyColumnSizes(proxyGridOrgans);
+}
+
+void GOSelectOrganDialog::CaptureAdditionalSizes(
+  GOAdditionalSizeKeeper &sizeKeeper) const {
+  GOAdditionalSizeKeeperProxy proxyGridOrgans(sizeKeeper, WX_GRID_ORGANS);
+
+  m_GridOrgans->CaptureColumnSizes(proxyGridOrgans);
+}
+
 bool GOSelectOrganDialog::TransferDataToWindow() {
-  for (unsigned i = 0, j = 0; j < r_OrganList.GetOrganList().size(); j++) {
-    const GOOrgan *o = r_OrganList.GetOrganList()[j];
+  int nRowsOld = m_GridOrgans->GetNumberRows();
+  unsigned rowN = 0;
 
+  if (nRowsOld > 0)
+    m_GridOrgans->DeleteRows(0, nRowsOld);
+  m_OrganPtrs.clear();
+  for (const auto o : r_OrganList.GetOrganList())
     if (o->IsUsable(r_OrganList)) {
-      const bool isArchive = o->GetArchiveID() != wxEmptyString;
+      const wxString &archiveId = o->GetArchiveID();
+      const bool isArchive = !archiveId.IsEmpty();
 
-      m_Organs->InsertItem(i, o->GetChurchName());
-      m_Organs->SetItemPtrData(i, (wxUIntPtr)o);
+      m_OrganPtrs.push_back(o);
+      m_GridOrgans->AppendRows(1);
+      m_GridOrgans->SetCellValue(rowN, GRID_COL_NAME, o->GetChurchName());
       if (isArchive) {
-        const GOArchiveFile *a = r_OrganList.GetArchiveByID(o->GetArchiveID());
+        const GOArchiveFile *a
+          = r_OrganList.GetArchiveByPath(o->GetArchivePath());
 
-        m_Organs->SetItem(i, 1, a ? a->GetName() : o->GetArchiveID());
+        m_GridOrgans->SetCellValue(
+          rowN, GRID_COL_PACKAGE, a ? a->GetName() : archiveId);
       }
-      m_Organs->SetItem(
-        i, 2, isArchive ? o->GetArchivePath() : o->GetODFPath());
-      i++;
+      m_GridOrgans->SetCellValue(
+        rowN, GRID_COL_PATH, isArchive ? o->GetArchivePath() : o->GetODFPath());
+      m_GridOrgans->SetCellAlignment(
+        rowN, GRID_COL_PATH, wxALIGN_RIGHT, wxALIGN_TOP);
+      rowN++;
     }
-  }
   return true;
+}
+
+void GOSelectOrganDialog::OnSelectCell(wxGridEvent &event) {
+  const int index = event.GetRow();
+  const wxArrayInt selectedRows = m_GridOrgans->GetSelectedRows();
+  bool isAlreadySelected = false;
+
+  for (auto rowN : selectedRows)
+    if (rowN == index)
+      isAlreadySelected = true;
+    else
+      m_GridOrgans->DeselectRow(rowN);
+  if (index >= 0 && !isAlreadySelected)
+    m_GridOrgans->SelectRow(index);
+}
+
+const GOOrgan *GOSelectOrganDialog::GetSelection() {
+  int index = m_GridOrgans->GetGridCursorRow();
+
+  return index >= 0 ? m_OrganPtrs[index] : nullptr;
 }
 
 bool GOSelectOrganDialog::Validate() {
   bool isValid = true;
-  long index = m_Organs->GetFirstSelected();
 
-  if (index < 0 || !m_Organs->GetItemData(index)) {
+  if (!GetSelection()) {
     wxMessageBox(
       _("Please select an organ"), _("Error"), wxOK | wxICON_ERROR, this);
     isValid = false;
   }
   return isValid;
-}
-
-const GOOrgan *GOSelectOrganDialog::GetSelection() {
-  return (const GOOrgan *)m_Organs->GetItemData(m_Organs->GetFirstSelected());
 }
