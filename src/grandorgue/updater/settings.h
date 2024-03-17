64f287a05e70287ec3b4d6554f696993cd996514
nanoufo <79045429+nanoufo@users.noreply.github.com>
2024-03-17 14:56:00 +0300

    Use GITHUB_PROJECT from CMake instead of hardcoding urls for update checking

--- src/grandorgue/updater/settings.h
+++ src/grandorgue/updater/settings.h
@@ -7,14 +7,16 @@
 #ifndef UPDATER_SETTINGS_H
 #define UPDATER_SETTINGS_H
 
+#include "go_defs.h"
+
 // Don't use long timeouts here since we wait for completion on application
 // exit. Also users may become annoyed if 'New release!' dialog appears while
 // they are playing.
 const long TIMEOUT_MS = 5000;
 const char *const USER_AGENT_HEADER = "User-Agent: GrandOrgue";
 const char *const LATEST_RELEASE_API_URL
-  = "https://api.github.com/repos/GrandOrgue/GrandOrgue/releases/latest";
+  = "https://api.github.com/repos/" GITHUB_PROJECT "/releases/latest";
 const char *const DOWNLOAD_URL
-  = "https://github.com/GrandOrgue/grandorgue/releases";
+  = "https://github.com/" GITHUB_PROJECT "/releases";
 
 #endif
