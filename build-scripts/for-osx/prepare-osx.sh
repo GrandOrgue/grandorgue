791ac189ed206302d6aa76c9baac882ebb29bfda
willeke1234 <97431382+willeke1234@users.noreply.github.com>
2024-03-22 23:07:29 +0100

    Fixes for ARM build

--- build-scripts/for-osx/prepare-osx.sh
+++ build-scripts/for-osx/prepare-osx.sh
@@ -14,7 +14,7 @@ brew install \
 brew link gettext --force
 
 # A workaround of https://gitlab.kitware.com/cmake/cmake/-/issues/23826
-for F in $(grep -l '(, weak)\?'  /usr/local/Cellar/cmake/*/share/cmake/Modules/GetPrerequisites.cmake); do
+for F in $(grep -l '(, weak)\?'  {/usr/local,/opt/homebrew}/Cellar/cmake/*/share/cmake/Modules/GetPrerequisites.cmake); do
   echo Patching $F...
   # OSx sed does not support the -i option
   sed -e 's/(, weak)\?/(, (weak|reexport))?/' $F >${F}.new
