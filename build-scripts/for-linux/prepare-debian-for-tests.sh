cb86a094b947c6104c3aa9105bad6e4209973157
Denis Roussel <denis.roussel@acsone.eu>
2024-04-13 15:41:25 +0200

    [IMP] Tests: Don't use git to install cpptrace

--- build-scripts/for-linux/prepare-debian-for-tests.sh
+++ build-scripts/for-linux/prepare-debian-for-tests.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -e
+
 # Install requirements for tests infrastructure
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git gcovr
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gcovr
 
 # Install cpptrace from sources
+CPP_TRACE="0.4.1"
+CPP_TRACE_VERSION="v$CPP_TRACE"
 mkdir cpptrace
 cd cpptrace
-git clone https://github.com/jeremy-rifkin/cpptrace.git .
-git checkout v0.4.0
+wget https://github.com/jeremy-rifkin/cpptrace/archive/refs/tags/$CPP_TRACE_VERSION.tar.gz
+tar -xf $CPP_TRACE_VERSION.tar.gz
+cd cpptrace-$CPP_TRACE
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
