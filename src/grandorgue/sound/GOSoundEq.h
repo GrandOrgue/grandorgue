a4098fea80eff0397c2aaefff009931a547e8c6e
larspalo <larspalo@yahoo.se>
2024-04-15 22:55:23 +0200

    Implemented filtering as 1st order versions.

--- src/grandorgue/sound/GOSoundEq.h
+++ src/grandorgue/sound/GOSoundEq.h
@@ -16,12 +16,9 @@ private:
   // Calculated filter coefficients
   double m_B0;
   double m_B1;
-  double m_B2;
   double m_A1;
-  double m_A2;
 
-  float m_state1[2];
-  float m_state2[2];
+  float m_state[2];
 
   bool m_isToApply;
 
@@ -34,51 +31,40 @@ public:
 inline void GOSoundEq::InitEq(int slider, unsigned samplerate) {
   m_B0 = 0;
   m_B1 = 0;
-  m_B2 = 0;
   m_A1 = 0;
-  m_A2 = 0;
   for (int i = 0; i < 2; i++) {
-    m_state1[i] = 0;
-    m_state2[i] = 0;
+    m_state[i] = 0;
   }
   m_isToApply = false;
 
   if (slider == 0) {
     return;
   } else {
-    double a0, a1, a2, b0, b1, b2;
+    double a0, a1, b0, b1;
     if (slider < 0) {
       // lowpass filter act as a high cut
       double hz = 4100 + slider * 40;
       double w0 = 2.0 * 3.14159265358979323846 * hz / samplerate;
       double cosW0 = cos(w0);
       double sinW0 = sin(w0);
-      double alpha = sinW0 / (2 * (1.0 / sqrt(2.0)));
-      b0 = (1.0 - cosW0) / 2.0;
-      b1 = 1.0 - cosW0;
-      b2 = (1.0 - cosW0) / 2.0;
-      a0 = 1.0 + alpha;
-      a1 = -2.0 * cosW0;
-      a2 = 1.0 - alpha;
+      b0 = sinW0;
+      b1 = sinW0;
+      a0 = sinW0 + cosW0 + 1.0;
+      a1 = (sinW0 - cosW0 - 1.0);
     } else {
       // highpass filter act as a low cut
       double hz = 20 + slider * 20;
       double w0 = 2.0 * 3.14159265358979323846 * hz / samplerate;
       double cosW0 = cos(w0);
       double sinW0 = sin(w0);
-      double alpha = sinW0 / (2 * (1.0 / sqrt(2.0)));
-      b0 = (1.0 + cosW0) / 2.0;
+      b0 = 1.0 + cosW0;
       b1 = -(1.0 + cosW0);
-      b2 = (1.0 + cosW0) / 2.0;
-      a0 = 1.0 + alpha;
-      a1 = -2.0 * cosW0;
-      a2 = 1.0 - alpha;
+      a0 = sinW0 + cosW0 + 1.0;
+      a1 = (sinW0 - cosW0 - 1.0);
     }
     m_B0 = b0 / a0;
     m_B1 = b1 / a0;
-    m_B2 = b2 / a0;
     m_A1 = a1 / a0;
-    m_A2 = a2 / a0;
     m_isToApply = true;
   }
 }
@@ -86,12 +72,10 @@ inline void GOSoundEq::InitEq(int slider, unsigned samplerate) {
 inline void GOSoundEq::ProcessBuffer(unsigned n_blocks, float *buffer) {
   float out[2];
   for (unsigned int i = 0; i < n_blocks; i++, buffer += 2) {
-    out[0] = m_B0 * buffer[0] + m_state1[0];
-    out[1] = m_B0 * buffer[1] + m_state1[1];
-    m_state1[0] = m_B1 * buffer[0] - m_A1 * out[0] + m_state2[0];
-    m_state1[1] = m_B1 * buffer[1] - m_A1 * out[1] + m_state2[1];
-    m_state2[0] = m_B2 * buffer[0] - m_A2 * out[0];
-    m_state2[1] = m_B2 * buffer[1] - m_A2 * out[1];
+    out[0] = m_B0 * buffer[0] + m_state[0];
+    out[1] = m_B0 * buffer[1] + m_state[1];
+    m_state[0] = m_B1 * buffer[0] - m_A1 * out[0];
+    m_state[1] = m_B1 * buffer[1] - m_A1 * out[1];
 
     buffer[0] = out[0];
     buffer[1] = out[1];
