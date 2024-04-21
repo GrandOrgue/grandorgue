e899fc3fa43ebe89a64fa1fc2bb9837559e46e43
d-musique <119169473+d-musique@users.noreply.github.com>
2024-04-21 19:51:21 +0200

    Allowed loading non standard compliant WAV files which lack zero padding of last chunk (#1890)
    If the file contains an odd-sized last chunk, the WAV file would
    sometimes omit the padding byte in non-standard cases.
    This change lets GOWave accept files which have this defect.
    
    ---------
    
    Co-authored-by: d-musique <d-musique@users.noreply.github.com>

--- src/core/GOWave.cpp
+++ src/core/GOWave.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -256,8 +256,11 @@ void GOWave::Open(const GOBuffer<uint8_t> &content, const wxString fileName) {
         check_for_bounds(fileName, header, offset, length, chunkOffset);
         LoadSamplerChunk(ptr + offset, size);
       }
-      /* Move to next chunk respecting word alignment */
-      offset += size + (size & 1);
+      /* Move to next chunk respecting word alignment
+         Unless lack of final padding (non spec-compliant) */
+      offset += size;
+      if (offset < length)
+        offset += size & 1;
     }
 
     if (offset != length)
