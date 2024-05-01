d2cecf4beac407f217fc0bba6d92a750d6101af7
Denis Roussel <denis.roussel@acsone.eu>
2024-05-01 12:17:05 +0200

    [IMP] Tests: Add a shell script to gather the testing commands

---
+++ build-scripts/for-linux/do-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# $1 - optional - [test | coverage | gcovr] - If nothing is provided,
+# run all the tests steps.
+
+set -e
+
+STEP=$1
+SCRIPT=""
+
+TEST_SCRIPT="ctest -T test --verbose"
+COVERAGE_SCRIPT="ctest -T coverage"
+GCOVR_SCRIPT="gcovr -e 'submodules/*|usr/*'"
+
+case $STEP in
+    "test") 
+        SCRIPT=$TEST_SCRIPT
+    ;;
+    "coverage")
+        SCRIPT=$COVERAGE_SCRIPT
+    ;;
+    "gcovr")
+        SCRIPT=$GCOVR_SCRIPT
+    ;;
+    "")
+        SCRIPT="$TEST_SCRIPT && $COVERAGE_SCRIPT && $GCOVR_SCRIPT"
+esac;
+
+$SCRIPT
