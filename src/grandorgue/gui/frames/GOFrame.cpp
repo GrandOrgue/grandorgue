598db3a6495a034146288b398f659489d54266ea
Denis Roussel <denis.roussel@acsone.eu>
2024-08-11 17:23:52 +0200

    [IMP] Add wxMaterialDesign library

--- src/grandorgue/gui/frames/GOFrame.cpp
+++ src/grandorgue/gui/frames/GOFrame.cpp
@@ -47,6 +47,8 @@
 #include "temperaments/GOTemperament.h"
 #include "threading/GOMutexLocker.h"
 
+#include "icons/GOIconManager.h"
+
 #include "GOApp.h"
 #include "GODocument.h"
 #include "GOEvent.h"
@@ -297,9 +299,14 @@ GOFrame::GOFrame(
     999);
   m_ToolBar->AddControl(m_SetterPosition);
   m_SetterPosition->SetValue(0);
-
+  GOIconManager *m_icon = new GOIconManager();
+  std::string volume = "volume";
   m_ToolBar->AddTool(
-    ID_VOLUME, _("&Volume"), GetImage_volume(), _("Volume"), wxITEM_NORMAL);
+    ID_VOLUME,
+    _("&Volume"),
+    m_icon->GetIcon(volume),
+    _("Volume"),
+    wxITEM_NORMAL);
   m_Volume = new wxSpinCtrl(
     m_ToolBar,
     ID_METER_AUDIO_SPIN,
---
+++ src/grandorgue/icons/GOIconManager.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
+ * License GPL-2.0 or later
+ * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+ */
+
+#include "GOIconManager.h"
+
+GOIconManager::GOIconManager(){};
+
+GOIconManager::~GOIconManager(){};
