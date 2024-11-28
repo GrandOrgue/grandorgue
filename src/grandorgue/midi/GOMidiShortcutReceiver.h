ca5af2f116df9b07edf134e1c30cd68c376d9ea0
Oleg Samarin <osamarin68@gmail.com>
2024-11-28 21:43:04 +0300

    Removed some unused class members

--- src/grandorgue/midi/GOMidiShortcutReceiver.h
+++ src/grandorgue/midi/GOMidiShortcutReceiver.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -17,9 +17,6 @@ class GOConfigWriter;
 class GOOrganController;
 
 class GOMidiShortcutReceiver : public GOMidiShortcutPattern {
-private:
-  GOOrganController *m_OrganController;
-
 public:
   GOMidiShortcutReceiver(KEY_RECEIVER_TYPE type)
     : GOMidiShortcutPattern(type) {}
