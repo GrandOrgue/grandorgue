9fc9568023bdfbd46b9d83ca2e939fcb368c3eb7
Oleg Samarin <osamarin68@gmail.com>
2024-11-29 22:18:21 +0300

    Fixed compiling warnining about clearing a non-trivial type https://github.com/GrandOrgue/grandorgue/issues/2001

--- src/grandorgue/sound/GOSoundSamplerPool.cpp
+++ src/grandorgue/sound/GOSoundSamplerPool.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -53,7 +53,7 @@ GOSoundSampler *GOSoundSamplerPool::GetSampler() {
       m_SamplerCount.fetch_add(1);
   }
   if (sampler)
-    memset(sampler, 0, sizeof(GOSoundSampler));
+    memset((void *)sampler, 0, sizeof(GOSoundSampler));
   return sampler;
 }
 
