5ece4bb1f214576388c101e03d5af5a1058f7bb5
Oleg Samarin <osamarin68@gmail.com>
2024-11-30 15:26:22 +0300

    Changed copyright headers in the cmake files

--- cmake/CopyDependencies.cmake
+++ cmake/CopyDependencies.cmake
@@ -1,8 +1,5 @@
-# GrandOrgue - free pipe organ simulator
-# 
 # Copyright 2006 Milan Digital Audio LLC
-# Copyright 2006 Milan Digital Audio LLC
-# Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+# Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
 # License GPL-2.0 or later
 # (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
 
--- cmake/SignMacOSApp.cmake
+++ cmake/SignMacOSApp.cmake
@@ -1,3 +1,8 @@
+# Copyright 2006 Milan Digital Audio LLC
+# Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
+# License GPL-2.0 or later
+# (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+
 # Script for macOS code signing
 
 if(NOT DEFINED APP_DIR)
