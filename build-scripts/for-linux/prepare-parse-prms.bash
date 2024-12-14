14411babc8338da1c9f6447826a9fdf7c338c204
Oleg Samarin <osamarin68@gmail.com>
2024-12-14 18:34:15 +0300

    Fixed build errors

--- build-scripts/for-linux/prepare-parse-prms.bash
+++ build-scripts/for-linux/prepare-parse-prms.bash
@@ -5,20 +5,20 @@ INSTALL_TESTS=notests
 
 # parse parameters
 while [[ $# -gt 0 ]]; do
-  case $1 in
-    wx30 | wx32)
-      WX_VER=$1
-      ;;
-    tests | notests)
-      INSTALL_TESTS=$1
-      ;;
-    "")
-      ;;
-    *)
-      echo "Unknown parameter $1" >&2
-      echo "Usage: $(basename $0) [wx30 | wx32] [tests | notests]"
-      exit 1
-      ;;
-  esac
+  if [[ -n "$1" ]]; then
+    case $1 in
+      wx30 | wx32)
+	WX_VER=$1
+	;;
+      tests | notests)
+	INSTALL_TESTS=$1
+	;;
+      *)
+	echo "Unknown parameter $1" >&2
+	echo "Usage: $(basename $0) [wx30 | wx32] [tests | notests]"
+	exit 1
+	;;
+    esac
+  fi
   shift # past a parameter
 done
