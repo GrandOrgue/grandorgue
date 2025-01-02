5942729074a2c626bc95055e504d8fed105e6c4e
Oleg Samarin <osamarin68@gmail.com>
2025-01-02 21:14:57 +0300

    Increased the maximum number of user-defined temperaments to 999 https://github.com/GrandOrgue/grandorgue/issues/1982

--- src/core/go_limits.h
+++ src/core/go_limits.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2025 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -13,6 +13,6 @@
 #define MAX_POLYPHONY 100000
 #define MAX_MIDI_DEVICES 50000
 #define MAX_SAMPLE_LENGTH 158760000
-#define MAX_TEMPERAMENTS 10
+#define MAX_TEMPERAMENTS 999 // limited by the format of setting keys
 
 #endif
