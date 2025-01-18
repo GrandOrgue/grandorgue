2e7e643993f69af9b851fd2e665bdc9b37950871
Oleg Samarin <osamarin68@gmail.com>
2025-01-18 20:24:59 +0300

    Added GOMidiObjectWithShortcut

---
+++ src/grandorgue/midi/objects/GOMidiObjectWithShortcut.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006 Milan Digital Audio LLC
+ * Copyright 2009-2025 GrandOrgue contributors (see AUTHORS)
+ * License GPL-2.0 or later
+ * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+ */
+
+#ifndef GOMIDIOBJECTWITHSHORTCUT_H
+#define GOMIDIOBJECTWITHSHORTCUT_H
+
+#include "midi/GOMidiShortcutReceiver.h"
+
+#include "GOMidiReceivingSendingObject.h"
+
+class GOMidiObjectWithShortcut : public GOMidiReceivingSendingObject {
+private:
+  GOMidiShortcutReceiver m_ShortcutReceiver;
+
+  void HandleKey(int key) override;
+
+protected:
+  GOMidiObjectWithShortcut(
+    GOOrganModel &organModel,
+    const wxString &midiTypeCode,
+    const wxString &midiTypeName,
+    GOMidiSenderType senderType,
+    GOMidiReceiverType receiverType,
+    GOMidiShortcutReceiver::ReceiverType shortcutType);
+
+  virtual ~GOMidiObjectWithShortcut();
+
+  void LoadMidiObject(
+    GOConfigReader &cfg, const wxString &group, GOMidiMap &midiMap) override;
+  void SaveMidiObject(
+    GOConfigWriter &cfg, const wxString &group, GOMidiMap &midiMap) override;
+
+  virtual void OnShortcutKeyReceived(
+    GOMidiShortcutReceiver::MatchType matchType, int key)
+    = 0;
+
+public:
+  void SetShortcutKey(unsigned key) { m_ShortcutReceiver.SetShortcut(key); }
+};
+
+#endif /* GOMIDIOBJECTWITHSHORTCUT_H */
