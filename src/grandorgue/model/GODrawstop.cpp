64b26a763645982d8a4414111c0238ac81bae31a
Oleg Samarin <osamarin68@gmail.com>
2025-01-19 12:50:49 +0300

    Renamed file GODrawStop to GODrawstop

--- src/grandorgue/model/GODrawstop.cpp
+++ src/grandorgue/model/GODrawstop.cpp
@@ -1,11 +1,11 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2024 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2025 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
 
-#include "GODrawStop.h"
+#include "GODrawstop.h"
 
 #include <wx/intl.h>
 
