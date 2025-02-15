b65c9d8ac3e4856cc8f859bb759f48cb6de33248
Oleg Samarin <osamarin68@gmail.com>
2025-02-15 20:34:09 +0300

    Eliminated writting default value of midi setup to the cmb file https://github.com/GrandOrgue/grandorgue/issues/1199

--- src/grandorgue/midi/GOMidiSender.h
+++ src/grandorgue/midi/GOMidiSender.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2025 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -30,8 +30,8 @@ public:
 
   void SetElementID(int id);
 
-  void Load(GOConfigReader &cfg, wxString group, GOMidiMap &map);
-  void Save(GOConfigWriter &cfg, wxString group, GOMidiMap &map);
+  void Load(GOConfigReader &cfg, const wxString &group, GOMidiMap &map);
+  void Save(GOConfigWriter &cfg, const wxString &group, GOMidiMap &map);
 
   void SetDisplay(bool state);
   void SetKey(unsigned key, unsigned velocity);
