ce7d23274112cd99a86e2286f7e25cf18ded5ed8
Oleg Samarin <osamarin68@gmail.com>
2025-03-29 14:44:38 +0300

    Introduced a class GORegisteredOrgan

--- src/core/GOOrganList.h
+++ src/core/GOOrganList.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2025 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -12,11 +12,9 @@
 
 #include "ptrvector.h"
 
-class GOArchiveFile;
-class GOConfigReader;
-class GOConfigWriter;
-class GOMidiMap;
-class GOOrgan;
+#include "archive/GOArchiveFile.h"
+
+#include "GOOrgan.h"
 
 class GOOrganList {
 private:
@@ -24,22 +22,26 @@ private:
   ptr_vector<GOArchiveFile> m_ArchiveList;
 
 protected:
-  void Load(GOConfigReader &cfg, GOMidiMap &map);
-  void Save(GOConfigWriter &cfg, GOMidiMap &map);
+  virtual GOOrgan *CloneOrgan(const GOOrgan &newOrgan) const;
+  void AddNewOrgan(GOOrgan *pOrgan) { m_OrganList.push_back(pOrgan); }
+  void AddNewArchive(GOArchiveFile *pArchive) {
+    m_ArchiveList.push_back(pArchive);
+  }
 
 public:
-  GOOrganList();
-  ~GOOrganList();
-
+  void ClearOrgans() { m_OrganList.clear(); }
   void AddOrgan(const GOOrgan &organ);
   void RemoveInvalidTmpOrgans();
   const ptr_vector<GOOrgan> &GetOrganList() const { return m_OrganList; }
   ptr_vector<GOOrgan> &GetOrganList() { return m_OrganList; }
   std::vector<const GOOrgan *> GetLRUOrganList();
 
+  void ClearArchives() { m_ArchiveList.clear(); }
   void AddArchive(const GOArchiveFile &archive);
-  ptr_vector<GOArchiveFile> &GetArchiveList();
-  const ptr_vector<GOArchiveFile> &GetArchiveList() const;
+  ptr_vector<GOArchiveFile> &GetArchiveList() { return m_ArchiveList; }
+  const ptr_vector<GOArchiveFile> &GetArchiveList() const {
+    return m_ArchiveList;
+  }
   const GOArchiveFile *GetArchiveByID(
     const wxString &id, bool useable = false) const;
   const GOArchiveFile *GetArchiveByPath(const wxString &path) const;
