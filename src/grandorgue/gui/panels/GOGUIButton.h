bf83741684b3baac21c531485e794861bb1fa460
Oleg Samarin <osamarin68@gmail.com>
2025-06-03 21:05:44 +0300

    Introduced class GOGUIMidiControl

--- src/grandorgue/gui/panels/GOGUIButton.h
+++ src/grandorgue/gui/panels/GOGUIButton.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2006 Milan Digital Audio LLC
- * Copyright 2009-2023 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2009-2025 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -13,11 +13,11 @@
 #include "primitives/GOBitmap.h"
 #include "primitives/GOFont.h"
 
-#include "GOGUIControl.h"
+#include "GOGUIMidiControl.h"
 
 class GOButtonControl;
 
-class GOGUIButton : public GOGUIControl {
+class GOGUIButton : public GOGUIMidiControl {
 protected:
   bool m_IsPiston;
   bool m_DispKeyLabelOnLeft;
