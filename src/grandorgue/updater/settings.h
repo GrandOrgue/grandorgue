dde9acad1dfab5739981553737800a2543118d9c
Oleg Samarin <osamarin68@gmail.com>
2026-01-10 16:44:04 +0300

    Fixed unability to check for updates on slow internet connections

--- src/grandorgue/updater/settings.h
+++ src/grandorgue/updater/settings.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 GrandOrgue contributors (see AUTHORS)
+ * Copyright 2024-2026 GrandOrgue contributors (see AUTHORS)
  * License GPL-2.0 or later
  * (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
  */
@@ -9,10 +9,11 @@
 
 #include "go_defs.h"
 
-const long TIMEOUT_MS = 5000;
+const long LOW_SPEED_TIME = 10;
+const long LOW_SPEED_LIMIT = 1024; // bytes per LOW_SPEED_TIME seconds
 const char *const USER_AGENT_HEADER = "User-Agent: GrandOrgue";
 const char *const RELEASES_API_URL
-  = "https://api.github.com/repos/" GITHUB_PROJECT "/releases?per_page=100";
+  = "https://api.github.com/repos/" GITHUB_PROJECT "/releases?per_page=3";
 const char *const DOWNLOAD_URL
   = "https://github.com/" GITHUB_PROJECT "/releases";
 
