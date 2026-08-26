oleg68/GrandOrgue-official

Moved ownership of the SoundRecorderTask from GOSoundSystem to GOSoundEngine

src/tests/testing/sound/GOTestSoundOrganEngineBase.cpp
@@ -35,7 +35,7 @@ GOSoundOrganEngine &GOTestSoundOrganEngineBase::BuildAndStartEngine(
       N_OUTPUT_CHANNELS,
       defaultConfigs[0].channels));
 
-  engine.BuildAndStart(configs, N_SAMPLES_PER_BUFFER, SAMPLE_RATE, m_recorder);
+  engine.BuildAndStart(configs, N_SAMPLES_PER_BUFFER, SAMPLE_RATE);
 
   GOAssert(engine.IsWorking(), "Engine should be WORKING after BuildAndStart");
   GOAssert(!engine.IsUsed(), "Engine should not be USED after BuildAndStart");

GrandOrgue/grandorgue

src/tests/testing/sound/GOTestSoundOrganEngineBase.cpp
@@ -35,7 +35,7 @@ GOSoundOrganEngine &GOTestSoundOrganEngineBase::BuildAndStartEngine(
       N_OUTPUT_CHANNELS,
       defaultConfigs[0].channels));
 
-  engine.BuildEngine(configs, N_SAMPLES_PER_BUFFER, SAMPLE_RATE, m_recorder);
+  engine.BuildEngine(configs, N_SAMPLES_PER_BUFFER, SAMPLE_RATE);
   engine.StartEngine();
 
   GOAssert(engine.IsWorking(), "Engine should be WORKING after BuildEngine");